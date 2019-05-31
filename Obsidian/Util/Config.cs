cc0c2c64d1f1de1db6e7c3a059082bcd6a0ef72e
Craftplacer <craftplacer@outlook.de>
2019-05-31 23:03:05 +0200

    Use camel case for server config

--- Obsidian/Util/Config.cs
+++ Obsidian/Util/Config.cs
@@ -10,13 +10,13 @@ namespace Obsidian.Entities
         [JsonProperty("port")]
         public int Port = 25565;
 
-        [JsonProperty("server-count")]
+        [JsonProperty("serverCount")]
         public int ServerCount = 1; // for now this does nothing. every server port will be port + n
 
-        [JsonProperty("joinmessage")]
+        [JsonProperty("joinMessage")]
         public string JoinMessage = "Suck the bbq sauce on my §d§ltiddies"; // Message on join, because we haven't implemented the actual server yet.
 
-        [JsonProperty("online_mode")]
+        [JsonProperty("onlineMode")]
         public bool OnlineMode = true;
 
     }
