d778ad051b56bf4d069abc30500cb83db7396250
Craftplacer <craftplacer@outlook.de>
2019-06-04 22:43:25 +0200

    Create ISerializable.cs
    Class for serialization of things

---
+++ Obsidian/Util/ISerializable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsidian.Util
+{
+    public interface ISerializable
+    {
+        Task<byte[]> ToArrayAsync(); 
+    }
+}
