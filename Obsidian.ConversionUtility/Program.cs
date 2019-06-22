4558a6a21190d70d9961aed9a270806c7f64a750
Craftplacer <craftplacer@outlook.de>
2019-06-22 16:20:09 +0200

    Starting new project for converting data

---
+++ Obsidian.ConversionUtility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Obsidian.ConversionUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
