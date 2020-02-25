cff4a83a92a6f82f371fd3fd588c926020414599
Craftplacer <craftplacer@outlook.de>
2020-02-25 18:44:20 +0100

    Implement asynchronousy in PacketDebugStream

--- Obsidian/Util/Debug/PacketDebugStream.cs
+++ Obsidian/Util/Debug/PacketDebugStream.cs
@@ -2,31 +2,24 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Obsidian.Util.Debug
 {
     public class PacketDebugStream : Stream
     {
-        public Stream Target { get; }
+        public Stream BaseStream { get; }
 
         //TODO: Dispose of this when done
         public MemoryStream MemoryStream { get; set; } = new MemoryStream();
 
         public PacketDebugStream(Stream target)
         {
-            Target = target;
+            BaseStream = target;
         }
 
-        public override void Flush()
-        {
-            Target.Flush();
-        }
-
-        public override int Read(byte[] buffer, int offset, int count) => Target.Read(buffer, offset, count);
-
-        public override long Seek(long offset, SeekOrigin origin) => Target.Seek(offset, origin);
-
-        public override void SetLength(long value) => Target.SetLength(value);
+        
 
         public override void Write(byte[] buffer, int offset, int count)
         {
@@ -35,18 +28,48 @@ namespace Obsidian.Util.Debug
             PacketDebug.Append("", MemoryStream.ToArray());
             MemoryStream = new MemoryStream();
             
-            
-            Target.Write(buffer, offset, count);
+            BaseStream.Write(buffer, offset, count);
         }
 
-        public override bool CanRead => Target.CanRead;
-        public override bool CanSeek => Target.CanSeek;
-        public override bool CanWrite  => Target.CanWrite;
-        public override long Length => Target.Length;
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            await MemoryStream.WriteAsync(buffer, offset, count, cancellationToken);
+
+            PacketDebug.Append("", MemoryStream.ToArray());
+            MemoryStream = new MemoryStream();
+
+            await BaseStream.WriteAsync(buffer, offset, count, cancellationToken);
+        }
+
+        #region Unchanged
+        public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default) => BaseStream.WriteAsync(buffer, cancellationToken);
+        public override void Close() => BaseStream.Close();
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => BaseStream.ReadAsync(buffer, offset, count, cancellationToken);
+        public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default) => BaseStream.ReadAsync(buffer, cancellationToken);
+        public override Task FlushAsync(CancellationToken cancellationToken) => BaseStream.FlushAsync(cancellationToken);
+        public override int ReadByte() => BaseStream.ReadByte();
+        public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken) => BaseStream.CopyToAsync(destination, bufferSize, cancellationToken);
+        public override bool CanRead => BaseStream.CanRead;
+        public override bool CanSeek => BaseStream.CanSeek;
+        public override bool CanWrite => BaseStream.CanWrite;
+        public override long Length => BaseStream.Length;
         public override long Position
         {
-            get => Target.Position;
-            set => Target.Position = value;
+            get => BaseStream.Position;
+            set => BaseStream.Position = value;
         }
+
+        public override void Flush()
+        {
+            BaseStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count) => BaseStream.Read(buffer, offset, count);
+
+        public override long Seek(long offset, SeekOrigin origin) => BaseStream.Seek(offset, origin);
+
+        public override void SetLength(long value) => BaseStream.SetLength(value);
+        #endregion
+
     }
 }
