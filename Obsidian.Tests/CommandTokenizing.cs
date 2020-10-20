bd605c1c8ad6654d983130a7800fac9c4cc124cc
Ryan de Jonge <naamloosdt@gmail.com>
2020-10-20 22:14:14 +0200

    new parser needs small bug fix

---
+++ Obsidian.Tests/CommandTokenizing.cs
@@ -0,0 +1,22 @@
+﻿using Obsidian.CommandFramework;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Obsidian.Tests
+{
+    public class CommandTokenizing
+    {
+        [Fact]
+        public async Task TestTokenizing()
+        {
+            var message = "/test help help \"help help \\n\" help";
+
+            var cmd = new CommandParser("/");
+
+            cmd.IsCommandQualified(message, out string qualified);
+            var split = cmd.SplitQualifiedString(qualified);
+            
+        }
+
+    }
+}
