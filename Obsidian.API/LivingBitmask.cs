b092b8882820672591634c176bb298862f4fcee1
Seb-stian <sebastian.svobo@gmail.com>
2020-11-02 22:47:31 +0100

    Added IEntity and ILiving to Obsidian.API

---
+++ Obsidian.API/LivingBitmask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Obsidian.API
+{
+    [Flags]
+    public enum LivingBitMask : byte
+    {
+        None = 0x00,
+
+        HandActive = 0x01,
+        ActiveHand = 0x02,
+        InRiptideSpinAttack = 0x04
+    }
+}
---
+++ Obsidian.API/Pose.cs
@@ -0,0 +1,19 @@
+﻿namespace Obsidian.API
+{
+    public enum Pose
+    {
+        Standing,
+
+        FallFlying,
+
+        Sleeping,
+
+        Swimming,
+
+        SpinAttack,
+
+        Sneaking,
+
+        Dying
+    }
+}
