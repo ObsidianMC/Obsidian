e9f8874a2ae954f7c873d466bab34c0a9c7a7916
Czompi <czompi@czompi.hu>
2020-11-02 13:13:52 +0100

    Added coloring to console output
    It works like inside the client.

--- Obsidian/Logging/Logger.cs
+++ Obsidian/Logging/Logger.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Obsidian.Chat;
 using System;
 
 namespace Obsidian.Logging
@@ -68,7 +69,34 @@ namespace Obsidian.Logging
                 }
 
                 var message = formatter(state, exception);
-                Console.WriteLine(message);
+                var msgLst = message.Contains("§") ? message.Split("§") : new string[] { $"r{message}" };
+                if (message[0] != '§' && msgLst.Length > 1) msgLst[0] = $"r{msgLst[0]}";
+                foreach (var msg in msgLst)
+                {
+                    Console.ForegroundColor = msg[0].ToString().ToLower()[0] switch
+                    {
+                        '0' => ConsoleColor.Black,
+                        '1' => ConsoleColor.DarkBlue,
+                        '2' => ConsoleColor.DarkGreen,
+                        '3' => ConsoleColor.DarkCyan,
+                        '4' => ConsoleColor.DarkRed,
+                        '5' => ConsoleColor.DarkMagenta,
+                        '6' => ConsoleColor.DarkYellow,
+                        '7' => ConsoleColor.Gray,
+                        '8' => ConsoleColor.DarkGray,
+                        '9' => ConsoleColor.Blue,
+                        'a' => ConsoleColor.Green,
+                        'b' => ConsoleColor.Cyan,
+                        'c' => ConsoleColor.Red,
+                        'd' => ConsoleColor.Magenta,
+                        'e' => ConsoleColor.Yellow,
+                        'f' => ConsoleColor.White,
+                        'r' => ConsoleColor.White,
+                        _ => ConsoleColor.White
+                    };
+                    Console.Write(msg.Substring(1));
+                }
+                Console.WriteLine("");
 
                 if (exception != null)
                     Console.WriteLine(exception);
