c024758c2540293227391458628af6f8233ee79a
Seb-stian <sebastian.svobo@gmail.com>
2020-11-03 17:06:19 +0100

    Added ITextComponent.CreateNew

--- Obsidian/ServerImplementationRegistry.cs
+++ Obsidian/ServerImplementationRegistry.cs
@@ -14,6 +14,7 @@ namespace Obsidian
             registered = true;
 
             IChatMessage.createNew = () => new ChatMessage();
+            ITextComponent.createNew = () => new TextComponent();
         }
     }
 }
