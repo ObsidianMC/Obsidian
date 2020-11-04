ea0b06954fac50b14b31ee0a9b9c970cf0a9517c
Tides <losttides@gmail.com>
2020-11-04 13:28:01 -0500

    Removed old packet stuff

--- Obsidian/Net/Packets/Packet.cs
+++ Obsidian/Net/Packets/Packet.cs
@@ -1,32 +1,8 @@
 using Obsidian.Entities;
-using Obsidian.Util;
-using Obsidian.Util.Extensions;
-using System;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace Obsidian.Net.Packets
 {
-    /*public class Packet
-    {
-        internal byte[] data;
-
-        internal int id;
-
-        public Packet(int packetid) => this.id = packetid;
-
-        public Packet(int packetId, byte[] data)
-        {
-            this.id = packetId;
-            this.data = data;
-        }
-
-
-        protected virtual Task ComposeAsync(MinecraftStream stream) => Task.CompletedTask;
-
-        protected virtual Task HandleAsync(Server server, Player player) => Task.CompletedTask;
-    }*/
-
     public interface IPacket
     {
         int Id { get; }
