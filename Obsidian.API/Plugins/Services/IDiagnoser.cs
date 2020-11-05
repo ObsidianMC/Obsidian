9354f28d46966c5beae4bd192694844f111f6c22
Sebastián <sebastian.svobo@gmail.com>
2020-11-05 22:44:38 +0100

    Added Diagnoser service

---
+++ Obsidian.API/Plugins/Services/IDiagnoser.cs
@@ -0,0 +1,26 @@
+﻿using Obsidian.API.Plugins.Services.Common;
+using Obsidian.API.Plugins.Services.Diagnostics;
+using System.Security;
+
+namespace Obsidian.API.Plugins.Services
+{
+    /// <summary>
+    /// Represents a service used for process diagnoses.
+    /// </summary>
+    public interface IDiagnoser : ISecuredService
+    {
+        /// <summary>
+        /// Gets a new <see cref="IProcess"/> component and associates it with the currently active process.
+        /// </summary>
+        /// <returns>A new <see cref="IProcess"/> component associated with the process resource that is running the calling application.</returns>
+        /// <exception cref="SecurityException"></exception>
+        public IProcess GetProcess();
+
+        /// <summary>
+        /// Creates a new <see cref="IProcess"/> component for each process resource on the local computer.
+        /// </summary>
+        /// <returns>An array of type <see cref="IProcess"/> that represents all the process resources running on the local computer.</returns>
+        /// <exception cref="SecurityException"></exception>
+        public IProcess[] GetProcesses();
+    }
+}
