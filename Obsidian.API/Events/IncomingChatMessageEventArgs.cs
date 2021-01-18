3eb0f5393bdb199e435584f35372774081a3db6d
Czompi <czompi@czompi.hu>
2021-01-18 21:08:29 +0100

    Added Format to `IncomingChatMessageEventArgs`

--- Obsidian.API/Events/IncomingChatMessageEventArgs.cs
+++ Obsidian.API/Events/IncomingChatMessageEventArgs.cs
@@ -5,13 +5,19 @@
         /// <summary>
         /// The message that was sent.
         /// </summary>
-        public string Message { get; }
+        public string Message { get; set; }
+
+        /// <summary>
+        /// The message format.
+        /// </summary>
+        public string Format { get; set; }
 
         public bool Cancel { get; set; }
 
-        public IncomingChatMessageEventArgs(IPlayer player, string message) : base(player)
+        public IncomingChatMessageEventArgs(IPlayer player, string message, string format) : base(player)
         {
             this.Message = message;
+            this.Format = format;
         }
     }
 }
