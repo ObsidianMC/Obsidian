a9931b8630deb75dcf2b39aa4fd04e224022ada5
Sebastián <sebastian.svobo@gmail.com>
2021-02-20 16:28:16 +0100

    Updated RequestResponse

--- Obsidian/Net/Packets/Status/RequestResponse.cs
+++ Obsidian/Net/Packets/Status/RequestResponse.cs
@@ -13,13 +13,15 @@ namespace Obsidian.Net.Packets.Status
 
         public int Id => 0x00;
 
-        public RequestResponse()
+        public RequestResponse(string json)
         {
+            Json = json;
         }
 
-        public RequestResponse(string json) => this.Json = json;
-
-        public RequestResponse(ServerStatus status) => this.Json = JsonConvert.SerializeObject(status);
+        public RequestResponse(ServerStatus status)
+        {
+            Json = JsonConvert.SerializeObject(status);
+        }
 
         public Task WriteAsync(MinecraftStream stream) => Task.CompletedTask;
 
