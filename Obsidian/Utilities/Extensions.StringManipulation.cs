967306a79a71adf666e82e35535e25866fcd5209
Sebastián <sebastian.svobo@gmail.com>
2021-03-14 21:56:39 +0100

    Minor improvement to string utilities

--- Obsidian/Utilities/Extensions.StringManipulation.cs
+++ Obsidian/Utilities/Extensions.StringManipulation.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace Obsidian.Utilities
@@ -12,7 +13,10 @@ namespace Obsidian.Utilities
     {
         public static readonly Regex pattern = new(@"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+");
 
-        // Source: https://stackoverflow.com/a/1415187
+        /// <remarks>
+        /// This method is not and shouldn't be used in performance-critical sections.
+        /// Source: https://stackoverflow.com/a/1415187
+        /// </remarks>
         public static string GetDescription(this Enum value)
         {
             Type type = value.GetType();
@@ -34,14 +38,30 @@ namespace Obsidian.Utilities
 
         public static EnchantmentType ToEnchantType(this string source) => Enum.Parse<EnchantmentType>(source.Split(":")[1].Replace("_", ""), true);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsNullOrWhitespace(this string value) => string.IsNullOrWhiteSpace(value);
 
         public static string Capitalize(this string value)
         {
-            if (string.IsNullOrEmpty(value))
-                throw new NullReferenceException(nameof(value));
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (value.Length == 0)
+                return value;
+
+            if (value.Length < 1024)
+            {
+                Span<char> span = stackalloc char[value.Length];
+                span[0] = char.ToUpper(value[0]);
+
+                var valueSpan = value.AsSpan(1);
+                valueSpan.CopyTo(span[1..]);
+
+                return new string(span);
+            }
 
             return char.ToUpper(value[0]) + value[1..];
         }
