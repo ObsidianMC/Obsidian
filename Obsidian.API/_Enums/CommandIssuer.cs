1a95c5b0a45a874562cc2ef13e38f9d0fb503904
roxxel <64275777+roxxel@users.noreply.github.com>
2021-04-17 20:44:03 +0300

    Oops

---
+++ Obsidian.API/_Enums/CommandIssuer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsidian.API
+{
+    [Flags]
+    public enum CommandIssuers
+    {
+        None,
+        Client,
+        Console,
+        RemoteConsole,
+        Plugin,
+        Any = ~None
+
+    }
+}
