ca9c85c77f6336f7d36386aaedd2b0b578d5fb91
Sebastián <sebastian.svobo@gmail.com>
2021-05-07 16:17:22 +0200

    Added missing issuer to command tests

--- Obsidian.Tests/Commands.cs
+++ Obsidian.Tests/Commands.cs
@@ -28,21 +28,23 @@ namespace Obsidian.Tests
         {
             var cmd = new CommandHandler("/");
 
+            ICommandSender sender = new CommandSender(CommandIssuers.Console, player: null, logger: null);
+
             cmd.RegisterCommandClass<Command>(null, new Command());
 
-            await cmd.ProcessCommand(new CommandContext("/ping 69 hello", null, null, null));
+            await cmd.ProcessCommand(new CommandContext("/ping 69 hello", sender, null, null));
             Assert.Equal(69, Command.arg1out);
             Assert.Equal("hello", Command.arg2out);
 
-            await cmd.ProcessCommand(new CommandContext("/pong ping 420 bye", null, null, null));
+            await cmd.ProcessCommand(new CommandContext("/pong ping 420 bye", sender, null, null));
             Assert.Equal(420, Command.arg1out);
             Assert.Equal("bye", Command.arg2out);
 
-            await cmd.ProcessCommand(new CommandContext("/ping 12 12", null, null, null));
+            await cmd.ProcessCommand(new CommandContext("/ping 12 12", sender, null, null));
             Assert.Equal(69, Command.arg1out);
             Assert.Equal("bye", Command.arg2out);
 
-            await cmd.ProcessCommand(new CommandContext("/ping 69 hey bye", null, null, null));
+            await cmd.ProcessCommand(new CommandContext("/ping 69 hey bye", sender, null, null));
             Assert.Equal(69, Command.arg1out);
             Assert.Equal("bye", Command.arg2out);
         }
@@ -54,6 +56,7 @@ namespace Obsidian.Tests
 
             [Command("ping")]
             [CommandInfo(description: "ping")]
+            [IssuerScope(CommandIssuers.Any)]
             public async Task ping(CommandContext ctx, int arg1, int arg2)
             {
                 await Task.Yield();
@@ -61,6 +64,7 @@ namespace Obsidian.Tests
 
             [Command("ping")]
             [CommandInfo(description: "ping")]
+            [IssuerScope(CommandIssuers.Any)]
             public async Task ping(CommandContext ctx, int arg1, string arg2)
             {
                 await Task.Yield();
@@ -70,6 +74,7 @@ namespace Obsidian.Tests
 
             [Command("ping")]
             [CommandInfo(description: "ping")]
+            [IssuerScope(CommandIssuers.Any)]
             public async Task ping(CommandContext ctx, int arg1, string arg2, string arg3)
             {
                 await Task.Yield();
@@ -77,10 +82,12 @@ namespace Obsidian.Tests
 
             [CommandGroup("pong")]
             [CommandInfo(description: "pong")]
+            [IssuerScope(CommandIssuers.Any)]
             public class Pong
             {
                 [Command("ping")]
                 [CommandInfo(description: "ping")]
+                [IssuerScope(CommandIssuers.Any)]
                 public async Task ping(CommandContext ctx, int arg1, string arg2)
                 {
                     await Task.Yield();
