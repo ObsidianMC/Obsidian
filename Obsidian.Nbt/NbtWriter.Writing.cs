d7861e99a19bac851867754aad7826f82be4f70b
Tides <losttides@gmail.com>
2021-05-12 06:04:14 -0400

    Changed up original structure

---
+++ Obsidian.Nbt/NbtWriter.Writing.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Obsidian.Nbt
+{
+    public partial class NbtWriter
+    {
+        private void Write(NbtTagType tagType) => this.Write((byte)tagType);
+
+        private void Write(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return;
+
+            if (value.Length > short.MaxValue)
+                throw new InvalidOperationException($"value length must be less than {short.MaxValue}");
+
+            var buffer = Encoding.UTF8.GetBytes(value);
+
+            this.Write((short)buffer.Length);
+            this.BaseStream.Write(buffer);
+        }
+
+        private void Write(short value)
+        {
+            Span<byte> buffer = stackalloc byte[2];
+
+            BitConverter.TryWriteBytes(buffer, value);
+
+            if (BitConverter.IsLittleEndian)
+                buffer.Reverse();
+
+            this.BaseStream.Write(buffer);
+        }
+
+        private void Write(int value)
+        {
+            Span<byte> buffer = stackalloc byte[4];
+
+            BitConverter.TryWriteBytes(buffer, value);
+
+            if (BitConverter.IsLittleEndian)
+                buffer.Reverse();
+
+            this.BaseStream.Write(buffer);
+        }
+
+        private void Write(float value)
+        {
+            Span<byte> buffer = stackalloc byte[4];
+
+            BitConverter.TryWriteBytes(buffer, value);
+
+            if (BitConverter.IsLittleEndian)
+                buffer.Reverse();
+
+            this.BaseStream.Write(buffer);
+        }
+
+        private void Write(long value)
+        {
+            Span<byte> buffer = stackalloc byte[8];
+
+            BitConverter.TryWriteBytes(buffer, value);
+
+            if (BitConverter.IsLittleEndian)
+                buffer.Reverse();
+
+            this.BaseStream.Write(buffer);
+        }
+
+        private void Write(double value)
+        {
+            Span<byte> buffer = stackalloc byte[8];
+
+            BitConverter.TryWriteBytes(buffer, value);
+
+            if (BitConverter.IsLittleEndian)
+                buffer.Reverse();
+
+            this.BaseStream.Write(buffer);
+        }
+    }
+}
--- Obsidian.Nbt/NbtWriter.cs
+++ Obsidian.Nbt/NbtWriter.cs
@@ -1,157 +1,163 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Obsidian.Nbt
 {
-    //TODO: 
-    public sealed class NbtWriter : BinaryWriter
+    public sealed partial class NbtWriter : IDisposable, IAsyncDisposable
     {
-        private NbtTagType parentType;
-        private NbtTagType? listType;
+        private NbtTagType? expectedListType;
+
+        private int listSize;
+        private int listIndex;
+
+        public NbtTagType RootType { get; }
 
         public bool IsClosed { get; internal set; }
 
-        public NbtWriter(NbtTagType type = NbtTagType.Compound) : this(new MemoryStream(), type) { }
+        public Stream BaseStream { get; }
 
-        public NbtWriter(Stream outstream, NbtTagType type = NbtTagType.Compound) : base(outstream)
+        public NbtWriter(Stream outstream, NbtTagType rootType, string name)
         {
-            this.parentType = type;
+            this.BaseStream = outstream;
+            this.RootType = rootType;
+
+            this.Write(rootType);
+            this.Write(name);
         }
 
         public void WriteCompoundStart(string name = "")
         {
             this.Validate(name, NbtTagType.Compound);
 
-            this.Write((byte)NbtTagType.Compound);
+            this.Write(NbtTagType.Compound);
             if (!string.IsNullOrEmpty(name))
                 this.Write(name);
         }
 
-        public void WriteListStart(string name, NbtTagType listType)
+        public void WriteListStart(string name, NbtTagType listType, int length)
         {
+            this.Validate(name, NbtTagType.List);
 
+            this.listSize = length;
+            this.expectedListType = listType;
+
+            this.Write(NbtTagType.List);
+            if (!string.IsNullOrEmpty(name))
+                this.Write(name);
+
+            this.Write(listType);
+            this.Write(length);
+        }
+
+        public void EndList()
+        {
+            if ((this.listIndex + 1) < this.listSize)
+                throw new InvalidOperationException("List cannot end because its size is smaller than the pre-defined size.");
+
+            this.listSize = 0;
+            this.listIndex = 0;
+            this.expectedListType = null;
+        }
+
+        public void EndCompound()
+        {
+            this.Write(NbtTagType.End);
         }
 
         public void WriteString(string name, string value)
         {
             this.Validate(name, NbtTagType.String);
 
-            this.Write((byte)NbtTagType.String);
+            this.Write(NbtTagType.String);
             this.Write(name);
             this.Write(value);
         }
 
-        public void EndCompound()
+        public void WriteByte(string name, byte value)
         {
-            this.Write((byte)NbtTagType.End);
+            this.Validate(name, NbtTagType.Byte);
+
+            this.Write(NbtTagType.Byte);
+            this.Write(name);
+            this.Write(value);
+        }
+
+        public void WriteShort(string name, short value)
+        {
+            this.Validate(name, NbtTagType.Short);
+
+            this.Write(NbtTagType.Short);
+            this.Write(name);
+            this.Write(value);
+        }
+
+        public void WriteInt(string name, int value)
+        {
+            this.Validate(name, NbtTagType.Int);
+
+            this.Write(NbtTagType.Int);
+            this.Write(name);
+            this.Write(value);
+        }
+
+        public void WriteLong(string name, long value)
+        {
+            this.Validate(name, NbtTagType.Long);
+
+            this.Write(NbtTagType.Long);
+            this.Write(name);
+            this.Write(value);
+        }
+
+        public void WriteFloat(string name, float value)
+        {
+            this.Validate(name, NbtTagType.Float);
+
+            this.Write(NbtTagType.Float);
+            this.Write(name);
+            this.Write(value);
+        }
+
+        public void WriteDouble(string name, double value)
+        {
+            this.Validate(name, NbtTagType.Double);
+
+            this.Write(NbtTagType.Double);
+            this.Write(name);
+            this.Write(value);
         }
 
         public void Validate(string name, NbtTagType type)
         {
             if (this.IsClosed)
-                throw new InvalidOperationException("Cannot write any more tags. Parent tag has been closed.");
+                throw new InvalidOperationException("Cannot write any more tags. Writer has been closed.");
 
-            if (string.IsNullOrEmpty(name) && this.parentType == NbtTagType.Compound)
+            if (string.IsNullOrEmpty(name) && this.RootType == NbtTagType.Compound)
                 throw new ArgumentException("Tags inside a compound tag must have a name.");
 
-            if (this.parentType == NbtTagType.List && this.listType != type)
-                throw new InvalidOperationException($"Expected list type: {this.listType}. Got: {type}");
-            else if (!string.IsNullOrEmpty(name) && this.listType != null)
-                throw new InvalidOperationException("Tags inside lists must be nameless.");
-        }
-
-        #region overrides
-        public override void Write(string value)
-        {
-            if (string.IsNullOrEmpty(value))
+            if (this.RootType == NbtTagType.List)
             {
-                this.Write((byte)0);
-            }
-            else
-            {
-                if (value.Length > short.MaxValue)
-                    throw new InvalidOperationException($"value length must be less than {short.MaxValue}");
+                if (this.expectedListType != type)
+                    throw new InvalidOperationException($"Expected list type: {this.expectedListType}. Got: {type}");
+                else if (!string.IsNullOrEmpty(name))
+                    throw new InvalidOperationException("Tags inside lists must be nameless.");
+                else if ((this.listIndex + 1) > this.listSize)
+                    throw new IndexOutOfRangeException("Exceeded pre-defined list size");
 
-                var buffer = Encoding.UTF8.GetBytes(value);
-
-                this.Write((short)buffer.Length);
-                this.Write(buffer);
+                this.listIndex++;
             }
         }
 
-        public override void Write(byte value)
+        //TODO
+        public void TryFinish()
         {
-            Span<byte> buffer = stackalloc byte[1];
-
-            BitConverter.TryWriteBytes(buffer, value);
-
-            if (BitConverter.IsLittleEndian)
-                buffer.Reverse();
-
-            this.Write(buffer);
+           
         }
 
-        public override void Write(short value)
-        {
-            Span<byte> buffer = stackalloc byte[2];
+        public ValueTask DisposeAsync() => this.BaseStream.DisposeAsync();
+        public void Dispose() => this.BaseStream.Dispose();
 
-            BitConverter.TryWriteBytes(buffer, value);
-
-            if (BitConverter.IsLittleEndian)
-                buffer.Reverse();
-
-            this.Write(buffer);
-        }
-
-        public override void Write(int value)
-        {
-            Span<byte> buffer = stackalloc byte[4];
-
-            BitConverter.TryWriteBytes(buffer, value);
-
-            if (BitConverter.IsLittleEndian)
-                buffer.Reverse();
-
-            this.Write(buffer);
-        }
-
-        public override void Write(float value)
-        {
-            Span<byte> buffer = stackalloc byte[4];
-
-            BitConverter.TryWriteBytes(buffer, value);
-
-            if (BitConverter.IsLittleEndian)
-                buffer.Reverse();
-
-            this.Write(buffer);
-        }
-
-        public override void Write(long value)
-        {
-            Span<byte> buffer = stackalloc byte[8];
-
-            BitConverter.TryWriteBytes(buffer, value);
-
-            if (BitConverter.IsLittleEndian)
-                buffer.Reverse();
-
-            this.Write(buffer);
-        }
-
-        public override void Write(double value)
-        {
-            Span<byte> buffer = stackalloc byte[8];
-
-            BitConverter.TryWriteBytes(buffer, value);
-
-            if (BitConverter.IsLittleEndian)
-                buffer.Reverse();
-
-            this.Write(buffer);
-        }
-        #endregion
+        private record TagNode(NbtTagType Type);
     }
 }
