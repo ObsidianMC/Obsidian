1529c321972dfd3f0b9de9586a5be77b549ad739
Sebastian <sebastian.svobo@gmail.com>
2021-05-31 01:21:06 +0200

    Added UpdateHealth packet

---
+++ Obsidian/Net/Packets/Play/Clientbound/UpdateHealth.cs
@@ -0,0 +1,26 @@
+﻿using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Clientbound
+{
+    [ClientOnly]
+    public partial class UpdateHealth : IClientboundPacket
+    {
+        [Field(0)]
+        public float Health { get; set; }
+
+        [Field(1), VarLength]
+        public int Food { get; set; }
+
+        [Field(2)]
+        public float FoodSaturation { get; set; }
+
+        public int Id => 0x49;
+
+        public UpdateHealth(float health, int food, float foodSaturation)
+        {
+            Health = health;
+            Food = food;
+            FoodSaturation = foodSaturation;
+        }
+    }
+}
