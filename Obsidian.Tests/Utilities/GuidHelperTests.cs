761200a021b5a955ede98752afdcb8c0a8cb98c2
Sebastian <sebastian.svobo@gmail.com>
2021-05-31 14:50:23 +0200

    Squashed DaanV2.UUID.Net dependency

---
+++ Obsidian.Tests/Utilities/GuidHelperTests.cs
@@ -0,0 +1,52 @@
+﻿using Obsidian.Utilities;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Obsidian.Tests.Utilities
+{
+    public class GuidHelperTests
+    {
+        public static IEnumerable<object[]> Data => new string[][]
+        {
+            new[] { string.Empty },
+            new[] { "ABCDE" },
+            new[] { "0123456789ABCDEF" }
+        };
+
+        [Fact(DisplayName = "Passing null throws an exception")]
+        public void NullThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => GuidHelper.FromStringHash(null));
+            Assert.Throws<ArgumentNullException>(() => GuidHelper.FromStringHashCryptographic(null));
+        }
+
+        [MemberData(nameof(Data))]
+        [Theory(DisplayName = "Creating GUID from string hash")]
+        public void GuidFromStringHash(string text)
+        {
+            Guid guid = GuidHelper.FromStringHash(text);
+            Assert.NotEqual(Guid.Empty, guid);
+
+            Guid guid2 = GuidHelper.FromStringHash(text);
+            Assert.Equal(guid, guid2);
+
+            Guid guid3 = GuidHelper.FromStringHash(text + "_");
+            Assert.NotEqual(guid, guid3);
+        }
+
+        [MemberData(nameof(Data))]
+        [Theory(DisplayName = "Creating GUID from string hash using cryptography")]
+        public void GuidFromStringHashCryptographic(string text)
+        {
+            Guid guid = GuidHelper.FromStringHashCryptographic(text);
+            Assert.NotEqual(Guid.Empty, guid);
+
+            Guid guid2 = GuidHelper.FromStringHashCryptographic(text);
+            Assert.Equal(guid, guid2);
+
+            Guid guid3 = GuidHelper.FromStringHashCryptographic(text + "_");
+            Assert.NotEqual(guid, guid3);
+        }
+    }
+}
---
+++ Obsidian/Utilities/GuidHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace Obsidian.Utilities
+{
+    public static class GuidHelper
+    {
+        private static HashAlgorithm hashAlgorithm;
+
+        public static Guid FromStringHash(string text)
+        {
+            if (text is null)
+                throw new ArgumentNullException(nameof(text));
+
+            int hash = text.GetHashCode();
+
+            var i128 = new Int128
+            {
+                a = HashCode.Combine(hash, 0),
+                b = HashCode.Combine(hash, 256),
+                c = HashCode.Combine(hash, 65536),
+                d = HashCode.Combine(hash, 16777216)
+            };
+
+            return Unsafe.As<Int128, Guid>(ref i128);
+        }
+
+        public static Guid FromStringHashCryptographic(string text)
+        {
+            if (text is null)
+                throw new ArgumentNullException(nameof(text));
+
+            var i128 = new Int128();
+
+            hashAlgorithm ??= MD5.Create();
+
+            hashAlgorithm.TryComputeHash(text.AsByteSpan(), i128.AsSpan(), out _);
+
+            return Unsafe.As<Int128, Guid>(ref i128);
+        }
+
+        private static ReadOnlySpan<byte> AsByteSpan(this string text)
+        {
+            ref char charPtr = ref Unsafe.AsRef(text.GetPinnableReference());
+            ref byte ptr = ref Unsafe.As<char, byte>(ref charPtr);
+            return MemoryMarshal.CreateReadOnlySpan(ref ptr, text.Length * 2);
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct Int128
+        {
+            [FieldOffset(0)]
+            public int a;
+            [FieldOffset(4)]
+            public int b;
+            [FieldOffset(8)]
+            public int c;
+            [FieldOffset(12)]
+            public int d;
+
+            [FieldOffset(0)]
+            private byte start;
+
+            public Span<byte> AsSpan()
+            {
+                return MemoryMarshal.CreateSpan(ref start, 16);
+            }
+        }
+    }
+}
