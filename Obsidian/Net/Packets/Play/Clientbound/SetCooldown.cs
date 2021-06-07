ae8b40ee5b7c1edb6231c18f536071db144f1ac7
Ryan de Jonge <naamloosdt@gmail.com>
2021-06-07 19:39:09 +0200

    Set Cooldown Packet

---
+++ Obsidian/Net/Packets/Play/Clientbound/SetCooldown.cs
@@ -0,0 +1,21 @@
+﻿using Obsidian.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsidian.Net.Packets.Play.Clientbound
+{
+    [ClientOnly]
+    public partial class SetCooldown : IClientboundPacket
+    {
+        [Field(0), VarLength]
+        public int ItemId;
+
+        [Field(1), VarLength]
+        public int CooldownTicks;
+
+        public int Id => 0x16;
+    }
+}
