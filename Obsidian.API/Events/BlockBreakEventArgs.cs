05f687185081a1e336f146cbd4994d149edda3e9
Sebastian <sebastian.svobo@gmail.com>
2021-08-31 23:46:31 +0200

    Why are you doing this to me Tides

--- Obsidian.API/Events/BlockBreakEventArgs.cs
+++ Obsidian.API/Events/BlockBreakEventArgs.cs
@@ -3,14 +3,14 @@
     public class BlockBreakEventArgs : BlockEventArgs, ICancellable
     {
         /// <summary>
-        /// Player that has broken the block. If the block wasn't broken by a player, value of this property will be null.
+        /// Player that has broken the block.
         /// </summary>
-        public IPlayer? Player { get; }
+        public IPlayer Player { get; }
 
         /// <inheritdoc/>
         public bool Cancel { get; set; }
 
-        internal BlockBreakEventArgs(IServer server, IPlayer? player, Block block, Vector location) : base(server, block, location)
+        internal BlockBreakEventArgs(IServer server, IPlayer player, Block block, Vector location) : base(server, block, location)
         {
             Player = player;
         }
