1d91178008ec6457d47b014d3fe8a51267fffd2e
Tides <losttides@gmail.com>
2021-09-05 17:35:56 -0400

    Create BossBar interface and moved bossbar stuff to api

---
+++ Obsidian.API/_Interfaces/IBossBar.cs
@@ -0,0 +1,30 @@
+﻿using Obsidian.API.Boss;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Obsidian.API
+{
+    public interface IBossBar
+    {
+        public Guid Uuid { get; }
+
+        public bool HasPlayer(Guid uuid);
+
+        public Task AddPlayerAsync(Guid uuid);
+
+        public Task RemovePlayerAsync(Guid uuid);
+
+        public Task UpdateTitleAsync(ChatMessage newTitle);
+
+        public Task UpdateHealthAsync(float newHealth);
+
+        public Task UpdateColorAsync(BossBarColor newColor);
+
+        public Task UpdateDivisionAsync(BossBarDivisionType newDivision);
+
+        public Task AddFlagsAsync(BossBarFlags newFlags);
+
+        public Task RemoveFlagAsync(BossBarFlags flag);
+    }
+}
