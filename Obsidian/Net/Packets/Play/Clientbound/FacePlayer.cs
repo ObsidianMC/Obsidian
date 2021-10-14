175ef24fba7f4adc374bef46da230f9fcca083a6
Roxxel <warperthe77@gmail.com>
2021-10-15 01:50:08 +0300

    Implemented Face Player packet

---
+++ Obsidian/Net/Packets/Play/Clientbound/FacePlayer.cs
@@ -0,0 +1,43 @@
+﻿using Obsidian.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsidian.Net.Packets.Play.Clientbound
+{
+    public partial class FacePlayer : IPacket
+    {
+        public int Id => 0x37;
+
+        [Field(0), VarLength, ActualType(typeof(int))]
+        public AimType Aim { get; set; }
+        [Field(1)]
+        public double TargetX { get; set; }
+        [Field(2)]
+        public double TargetY { get; set; }
+        [Field(3)]
+        public double TargetZ { get; set; }
+
+        [Field(4)]
+        public bool IsEntity { get; set; }
+
+        [Field(5), VarLength, Condition(nameof(IsEntity))]
+        public int EntityId { get; set; }
+
+        [Field(6), VarLength, ActualType(typeof(int)), Condition(nameof(IsEntity))]
+        public AimType AimEntity { get; set; }
+
+        public FacePlayer()
+        {
+
+        }
+    }
+
+    public enum AimType : int
+    {
+        Feet = 0,
+        Eyes
+    }
+}
