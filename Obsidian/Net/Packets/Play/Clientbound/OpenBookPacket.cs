7b35569823253406d589007510670c136af600b7
Tides <losttides@gmail.com>
2021-10-18 01:40:28 -0400

    Create OpenBookPacket.cs

---
+++ Obsidian/Net/Packets/Play/Clientbound/OpenBookPacket.cs
@@ -0,0 +1,13 @@
+﻿using Obsidian.API;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Clientbound
+{
+    public partial class OpenBookPacket : IClientboundPacket
+    {
+        [Field(0), ActualType(typeof(int)), VarLength]
+        public Hand Hand { get; set; }
+
+        public int Id => 0x2D;
+    }
+}
