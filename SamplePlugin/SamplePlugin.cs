aedab14faea5e9c4548ca5827944114649bf8e25
Piotr Madura <ppiotr.madura@gmail.com>
2021-11-04 19:55:47 +0100

    Sample plugin for new framework
    Changed the sample plugin so that it is compatible with the new framework

--- SamplePlugin/SamplePlugin.cs
+++ SamplePlugin/SamplePlugin.cs
@@ -1,84 +1,60 @@
 ﻿using Obsidian.API;
 using Obsidian.API.Events;
 using Obsidian.API.Plugins;
+using Obsidian.API.Plugins.Events;
 using Obsidian.API.Plugins.Services;
 using System;
 using System.Threading.Tasks;
 
 namespace SamplePlugin
 {
-    [Plugin(Name = "Sample Plugin", Version = "1.0",
-            Authors = "Obsidian Team", Description = "My sample plugin.",
-            ProjectUrl = "https://github.com/Naamloos/Obsidian")]
-    public class SamplePlugin : PluginBase
+    public class SamplePlugin : Plugin
     {
-        // Any interface from Obsidian.Plugins.Services can be injected into properties
-        [Inject] public ILogger Logger { get; set; }
-        [Inject] public IFileReader FileReader { get; set; }
+        public override string Identifier => "org.example.SamplePlugin";
 
-        // Dependencies will be injected automatically, if dependency class and field/property names match
-        // Plugins won't load until all their required dependencies are added
-        // Optional dependencies may be injected at any time, if at all
-        [Dependency(MinVersion = "2.0", Optional = true), Alias("Sample Remote Plugin")]
-        public MyWrapper SampleRemotePlugin { get; set; }
+        public override PluginInfo Info { get; } = new() {
+            Name = "Sample Plugin",
+            Description = "A plugin to test the functionality of the plugin system",
+            Version = new Version(1, 0, 0, 0),
+            Authors = new[] {"Obsidian Team"},
+            ProjectUrl = new Uri("https://github.com/ObsidianMC/Obsidian")
+        };
 
-        // One of server messages, called when an event occurs
-        public void OnLoad(IServer server)
+        private ILogger logger;
+        private IServer server;
+        
+        public override void Initialize(IPluginServiceProvider serviceProvider)
         {
-            Logger.Log($"§a{Info.Name} §floaded! Hello §a{server.DefaultWorld.Name}§f!");
-            Logger.Log($"Hello! I live at §a{FileReader.CreateWorkingDirectory()}§f!");
+            logger = serviceProvider.GetLogger(this);
+            server = serviceProvider.GetServer();
+
+            logger.Log("Hello world!");
+            logger.Log($"§a{Info.Name} §floaded! Hello §a{server.DefaultWorld.Name}§f!");
         }
 
+        public override void DeInitialize()
+        {
+            logger.Log("Goodbye cruel world!");
+        }
+
+        [EventHandler(Event.PermissionRevoked)]
         public void OnPermissionRevoked(PermissionRevokedEventArgs args)
         {
-            Logger.Log($"Permission {args.Permission} revoked from player {args.Player.Username}");
+            logger.Log($"Permission {args.Permission} revoked from player {args.Player.Username}");
         }
 
+        [EventHandler(Event.PermissionGranted, EventPriority.Critical)]
         public void OnPermissionGranted(PermissionGrantedEventArgs args)
         {
-            Logger.Log($"Permission {args.Permission} granted to player {args.Player.Username}");
+            logger.Log($"Permission {args.Permission} granted to player {args.Player.Username}");
         }
 
-        public async Task OnPlayerJoin(PlayerJoinEventArgs playerJoinEvent)
+        [EventHandler(Event.PlayerJoin)]
+        public async Task OnPlayerJoinedAsync(PlayerJoinEventArgs args)
         {
-            var player = playerJoinEvent.Player;
+            var player = args.Player;
 
             await player.SendMessageAsync(ChatMessage.Simple($"Welcome {player.Username}!", ChatColor.Gold));
         }
-
-        [Command("plugincommand")]
-        [CommandInfo("woop dee doo this command is from within a plugin class!!")]
-        public async Task PluginCommandAsync(CommandContext ctx)
-        {
-            await ctx.Sender.SendMessageAsync("Hello from plugin command implemented in Plugin class!");
-        }
-    }
-
-    [CommandRoot]
-    public class MyCommands
-    {
-        [Inject]
-        public SamplePlugin Plugin { get; set; }
-
-        [Inject]
-        public ILogger Logger { get; set; }
-
-        [Command("mycommand")]
-        [CommandInfo("woop dee doo this command is from a plugin")]
-        public async Task MyCommandAsync(CommandContext ctx)
-        {
-            Plugin.Logger.Log("Testing Plugin as injected dependency");
-            Logger.Log("Testing Services as injected dependency");
-            await ctx.Player.SendMessageAsync("Hello from plugin command!");
-        }
-    }
-
-    public class MyWrapper : PluginWrapper
-    {
-        public Action Step { get; set; }
-        [Alias("get_StepCount")] private Func<int> GetStepCount { get; set; }
-        [Alias("set_StepCount")] private Action<int> SetStepCount { get; set; }
-
-        public int StepCount { get => GetStepCount(); set => SetStepCount(value); }
     }
 }
