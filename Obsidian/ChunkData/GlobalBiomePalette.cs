aa5f400d5ca914e29b54990d844185de9d40ceee
Seb-stian <47394098+Seb-stian@users.noreply.github.com>
2021-12-22 09:50:42 +0100

    Fix merge conflict

--- Obsidian/ChunkData/GlobalBiomePalette.cs
+++ Obsidian/ChunkData/GlobalBiomePalette.cs
@@ -1,21 +1,26 @@
 ﻿using Obsidian.Net;
 
-
 namespace Obsidian.ChunkData;
+
 public class GlobalBiomePalette : IPalette<Biomes>
 {
-    public int[] Values => throw new NotImplementedException();
-
-    public int Count => throw new NotImplementedException();
-
+    public int[] Values => throw new NotSupportedException();
+    public int Count => throw new NotSupportedException();
     public bool IsFull => false;
 
-    public int GetIdFromValue(Biomes biome) => (int)biome;
+    public bool TryGetId(Biomes biome, out int id)
+    {
+        id = (int)biome;
+        return true;
+    }
+
+    public int GetOrAddId(Biomes biome) => (int)biome;
 
     public Biomes GetValueFromIndex(int index) => (Biomes)index;
 
     public Task WriteToAsync(MinecraftStream stream) => Task.CompletedTask;
-
     public Task ReadFromAsync(MinecraftStream stream) => Task.CompletedTask;
-    public void WriteTo(MinecraftStream stream) { }
+    public void WriteTo(MinecraftStream stream)
+    {
+    }
 }
