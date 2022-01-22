c69e7621b8b5216fd1e92d6f754ea9158e3a6619
Dávid Czompó <czompi@czompi.hu>
2022-01-22 14:52:51 +0100

    Add UpdateJigsawBlock

---
+++ Obsidian/Net/Packets/Play/Serverbound/UpdateJigsawBlock.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class UpdateJigsawBlock : IServerboundPacket
+{
+    [Field(0)]
+    public VectorF Location { get; set; }
+    
+    [Field(1)]
+    public string Name { get; set; }
+    
+    [Field(2)]
+    public string Target { get; set; }
+    
+    [Field(3)]
+    public string Pool { get; set; }
+
+    [Field(4)]
+    public string FinalState { get; set; }
+
+    [Field(5)]
+    public string JointType { get; set; }
+
+    public int Id => 0x29;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+
+}
