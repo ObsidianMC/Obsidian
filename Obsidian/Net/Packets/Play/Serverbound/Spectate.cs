d2b0cc9c73aa456618595c3de6056f258f21f4de
Dávid Czompó <czompi@czompi.hu>
2022-01-22 15:09:40 +0100

    Add Spectate

---
+++ Obsidian/Net/Packets/Play/Serverbound/Spectate.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class Spectate : IServerboundPacket
+{
+    [Field(0)]
+    public Guid TargetPlayer { get; set; }
+
+    public int Id => 0x2D;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
