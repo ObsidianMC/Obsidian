47225866eb3d84e70c5c8d348fd52b2c97797fa6
Dávid Czompó <czompi@czompi.hu>
2022-01-22 14:53:09 +0100

    Add LockDifficulty

---
+++ Obsidian/Net/Packets/Play/Serverbound/LockDifficulty.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class LockDifficulty : IServerboundPacket
+{
+    [Field(0)]
+    public bool Locked { get; set; }
+
+    public int Id => 0x10;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
