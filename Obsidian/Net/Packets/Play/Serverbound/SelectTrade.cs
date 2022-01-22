e344dd32929e28baa53f5072f9765d36db7ca5b3
Dávid Czompó <czompi@czompi.hu>
2022-01-22 17:28:43 +0100

    Add SelectTrade

---
+++ Obsidian/Net/Packets/Play/Serverbound/SelectTrade.cs
@@ -0,0 +1,14 @@
+﻿using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class SelectTrade : IServerboundPacket
+{
+    [Field(0), VarLength]
+    public int SelectedSlot { get; set; }
+
+    public int Id => 0x23;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
