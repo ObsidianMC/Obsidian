d11a34262f30fc897fe78d782b9d728389bf0c82
Dávid Czompó <czompi@czompi.hu>
2022-01-22 14:53:01 +0100

    Add GenerateStructure

---
+++ Obsidian/Net/Packets/Play/Serverbound/GenerateStructure.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class GenerateStructure : IServerboundPacket
+{
+    [Field(0)]
+    public VectorF Position { get; set; }
+
+    [Field(1), VarLength]
+    public int Levels { get; set; }
+    
+    [Field(2)]
+    public bool KeepJigsaws { get; set; }
+
+    public int Id => 0x0E;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
