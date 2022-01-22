e5ab185a42400ee842539cb2ca87edef721650f9
Dávid Czompó <czompi@czompi.hu>
2022-01-22 14:51:46 +0100

    Add VehicleMove

---
+++ Obsidian/Net/Packets/Play/Serverbound/VehicleMove.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class VehicleMove : IServerboundPacket
+{
+    [Field(0), DataFormat(typeof(double))]
+    public VectorF Position { get; set; }
+
+    [Field(1)]
+    public float Yaw { get; set; }
+
+    [Field(2)]
+    public float Pitch { get; set; }
+
+    public int Id => 0x15;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
