18fcf03a9cf56024a79e17fefd5c12a4a9d8adb1
Dávid Czompó <czompi@czompi.hu>
2022-01-22 14:51:58 +0100

    Add SteerBoat

---
+++ Obsidian/Net/Packets/Play/Serverbound/SteerBoat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class SteerBoat : IServerboundPacket
+{
+    [Field(0)]
+    public bool LeftPaddleTurning { get; set; }
+    [Field(1)]
+    public bool RightPaddleTurning { get; set; }
+
+    public int Id => 0x16;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
