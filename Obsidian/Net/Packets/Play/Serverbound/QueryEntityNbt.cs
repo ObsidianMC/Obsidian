ec44b4acfeaeb55d091e754156bb00bd7676d7df
Dávid Czompó <czompi@czompi.hu>
2022-01-22 14:53:33 +0100

    Add QueryEntityNbt

---
+++ Obsidian/Net/Packets/Play/Serverbound/QueryEntityNbt.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class QueryEntityNbt : IServerboundPacket
+{
+    [Field(0), VarLength]
+    public int TransactionId { get; set; }
+    
+    [Field(1), VarLength]
+    public int EntityId { get; set; }
+    
+    public int Id => 0x15;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
