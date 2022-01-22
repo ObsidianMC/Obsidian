ecd06faa38bacb696a9569dbcc474d9ea94ca4fb
Dávid Czompó <czompi@czompi.hu>
2022-01-22 14:14:50 +0100

    Add LoginPluginRequest and LoginPluginResponse

---
+++ Obsidian/Net/Packets/Login/LoginPluginRequest.cs
@@ -0,0 +1,19 @@
+﻿using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Login;
+
+public partial class LoginPluginRequest : IClientboundPacket
+{
+    [Field(0), VarLength]
+    public int MessageId { get; set; }
+    
+    [Field(1)]
+    public string Channel { get; set; }
+    
+    [Field(2)]
+    public byte[] Data { get; set; }
+    
+    public int Id => 0x04;
+
+}
---
+++ Obsidian/Net/Packets/Login/LoginPluginResponse.cs
@@ -0,0 +1,25 @@
+﻿using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsidian.Net.Packets.Login;
+
+public partial class LoginPluginResponse : IServerboundPacket
+{
+    [Field(0), VarLength]
+    public int MessageId { get; set; }
+
+    [Field(1)]
+    public bool Successful { get; set; }
+
+    [Field(2)]
+    public byte[] Data { get; set; }
+
+    public int Id => 0x04;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
