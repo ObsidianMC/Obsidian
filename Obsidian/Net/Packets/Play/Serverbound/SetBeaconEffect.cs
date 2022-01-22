9ff885e6fa6b112d7b9e627d7a9ba2708336f6aa
Dávid Czompó <czompi@czompi.hu>
2022-01-22 17:28:51 +0100

    Add SetBeaconEffect

---
+++ Obsidian/Net/Packets/Play/Serverbound/SetBeaconEffect.cs
@@ -0,0 +1,17 @@
+﻿using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class SetBeaconEffect : IServerboundPacket
+{
+    [Field(0), VarLength]
+    public int PrimaryEffect { get; set; }
+    
+    [Field(1), VarLength]
+    public int SecondaryEffect { get; set; }
+
+    public int Id => 0x24;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
