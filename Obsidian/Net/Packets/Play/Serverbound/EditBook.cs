f245913d31f63d1d164e67ccb5144a834e2e9437
Dávid Czompó <czompi@czompi.hu>
2022-01-22 14:51:28 +0100

    Add EditBook

---
+++ Obsidian/Net/Packets/Play/Serverbound/EditBook.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Obsidian.Entities;
+using Obsidian.Serialization.Attributes;
+
+namespace Obsidian.Net.Packets.Play.Serverbound;
+
+public partial class EditBook : IServerboundPacket
+{
+    [Field(0), VarLength, ActualType(typeof(int))]
+    public Hand Hand { get; set; }
+    
+    [Field(1), VarLength]
+    public int Count { get; set; }
+    
+    [Field(2), ActualType(typeof(string))]
+    public List<string> Entries { get; set; }
+    
+    [Field(3)]
+    public bool HasTitle { get; set; }
+    
+    [Field(4)]
+    public string Title { get; set; }
+
+    public int Id => 0x15;
+
+    public ValueTask HandleAsync(Server server, Player player) => ValueTask.CompletedTask;
+}
