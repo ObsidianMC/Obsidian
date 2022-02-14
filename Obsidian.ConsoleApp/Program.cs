d286331631097bef96f4db583e6dade3349f00a8
Seb-stian <47394098+Seb-stian@users.noreply.github.com>
2022-02-14 20:24:27 +0100

    Merge master

--- Obsidian.ConsoleApp/Program.cs
+++ Obsidian.ConsoleApp/Program.cs
@@ -7,13 +7,15 @@ namespace Obsidian.ConsoleApp;
 
 public static class Program
 {
-    private static async Task Main()
+    private static async Task Main(string[] args)
     {
 #if RELEASE
         string version = "0.1";
 #else
         string version = "0.1-DEV";
-        Environment.CurrentDirectory = Path.GetDirectoryName(Environment.ProcessPath)!;
+        Environment.CurrentDirectory = args.Length > 0
+            ? string.Join(' ', args)
+            : Path.GetDirectoryName(Environment.ProcessPath)!;
 #endif
         CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
@@ -28,33 +30,13 @@ public static class Program
 
         string serverDirectory = string.Empty; // This resolves to the binary path by default.
 
-        string configPath = Path.Combine(serverDirectory, "config.json");
-        var configFile = new FileInfo(configPath);
-        Config config;
-
-        if (configFile.Exists)
-        {
-            using var fs = configFile.OpenRead();
-            config = await fs.FromJsonAsync<Config>();
-        }
-        else
-        {
-            config = new Config();
-
-            using var fs = configFile.Create();
-
-            await config.ToJsonAsync(fs);
-
-            CommandLine.WriteLine($"Created new configuration file for Server");
-            CommandLine.WriteLine($"Please fill in your config with the values you wish to use for your server.\n");
-            CommandLine.ForegroundColor = ConsoleColor.Green;
-            CommandLine.WriteLine(configPath);
-            CommandLine.ResetColor();
+        Config? config = await TryLoadConfigAsync();
+        if (config is null)
             return;
-        }
 
+        List<ServerWorld> worlds = await TryLoadServerWorldsAsync();
         var loggerProvider = new LoggerProvider((category, logLevel) => new Logger(category, logLevel));
-        var server = new Server(config, version, serverDirectory, loggerProvider);
+        var server = new Server(config, version, serverDirectory, worlds, loggerProvider);
 
         CommandLine.RegisterCommand("exit", (_, _) => StopServerAsync(server));
         CommandLine.RegisterCommand("e", (_, args) => ExecuteAsync(args, server));
@@ -75,6 +57,62 @@ public static class Program
     private static async ValueTask ExecuteAsync(ReadOnlyMemory<char> args, Server server)
     {
         await server.ExecuteCommand(args);
+    }
+
+    private static async Task<List<ServerWorld>> TryLoadServerWorldsAsync()
+    {
+        var worldsFile = new FileInfo("worlds.json");
+
+        if (worldsFile.Exists)
+        {
+            using var worldsFileStream = worldsFile.OpenRead();
+
+            return await worldsFileStream.FromJsonAsync<List<ServerWorld>>();
+        }
+
+        var worlds = new List<ServerWorld>()
+        {
+            new()
+            {
+                ChildDimensions =
+                {
+                    "minecraft:the_nether",
+                    "minecraft:the_end"
+                }
+            }
+        };
+
+        using var fs = worldsFile.Create();
+
+        await worlds.ToJsonAsync(fs);
+
+        return worlds;
+    }
+
+    private static async Task<Config?> TryLoadConfigAsync()
+    {
+        var configFile = new FileInfo("config.json");
+
+        if (configFile.Exists)
+        {
+            using var configFileStream = configFile.OpenRead();
+
+            return await configFileStream.FromJsonAsync<Config>();
+        }
+
+        var config = new Config();
+
+        using var fs = configFile.Create();
+
+        await config.ToJsonAsync(fs);
+
+        Console.WriteLine($"Created new configuration file for Server");
+        Console.WriteLine($"Please fill in your config with the values you wish to use for your server.\n");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(configFile.FullName);
+        Console.ResetColor();
+
+        return null;
     }
 
     // Cool startup console logo because that's cool
