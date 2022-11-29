1e89a3516b6d4506420b331be878f086b399f995
Tides <losttides@gmail.com>
2022-11-29 04:49:37 -0500

    Initial RegionFile.cs changes

--- Obsidian/WorldData/RegionFile.cs
+++ Obsidian/WorldData/RegionFile.cs
@@ -1,250 +1,254 @@
-﻿using System.Buffers;
+﻿using Microsoft.Extensions.Logging;
+using System.Buffers;
+using System.Buffers.Binary;
 using System.IO;
 using System.Threading;
 
 namespace Obsidian.WorldData;
 
-public class RegionFile : IAsyncDisposable
+public sealed class RegionFile : IAsyncDisposable
 {
+    private const int HeaderTableSize = 1024;
+    private const int SectionSize = 4096;
+
     private readonly string filePath;
+    private readonly int x;
+    private readonly int z;
     private readonly int cubicRegionSize;
-    private readonly int tableSize;
+    private readonly int op;
+    private readonly SemaphoreSlim semaphore = new(1, 1);
+
     private FileStream regionFileStream;
-    private RegionFileHeaderTable locationTable, timestampTable;
-    private bool disposedValue;
-    private readonly IMemoryOwner<byte> fileCache;
-    private int nextAvailableOffset;
+    private IMemoryOwner<byte> chunkCache;
+
+    private bool disposed;
+
+    public int[] Locations { get; private set; } = new int[HeaderTableSize];
+    public int[] Timestamps { get; private set; } = new int[HeaderTableSize];
 
     /// <summary>
     /// Reference Material: https://wiki.vg/Region_Files#Structure
     /// </summary>
     public RegionFile(string filePath, int cubicRegionSize = 32)
     {
-        this.filePath = filePath;
-        this.cubicRegionSize = cubicRegionSize;
-        this.tableSize = cubicRegionSize * cubicRegionSize * 4;
-        var minCacheSize = (tableSize * 2) + (cubicRegionSize * cubicRegionSize * (4096 + 4));
+        var name = Path.GetFileName(filePath);
+        var split = name.Split('.');
 
-        fileCache = MemoryPool<byte>.Shared.Rent(minCacheSize);
+        this.filePath = filePath;
+        this.x = int.Parse(split[1]);
+        this.z = int.Parse(split[2]);
+        this.cubicRegionSize = cubicRegionSize;
+
+        this.op = cubicRegionSize - 1;
     }
 
     public async Task<bool> InitializeAsync()
     {
-        if (!File.Exists(filePath))
-        {
-            await InitializeNewFileAsync();
-        }
-
         try
         {
-            regionFileStream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            this.regionFileStream = new(this.filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
         }
-        catch (IOException)
+        catch { throw; }
+
+        if (regionFileStream.Length == 0)
+            return true;
+
+        this.chunkCache = MemoryPool<byte>.Shared.Rent((int)this.regionFileStream.Length - SectionSize * 2);
+
+        for (var index = 0; index < HeaderTableSize; index++)
         {
-            return false;
+            using var num = new RentedArray<byte>(4);
+
+            await this.regionFileStream.ReadAsync(num);
+
+            this.Locations[index] = BinaryPrimitives.ReadInt32BigEndian(num);
         }
 
-        // Load file into memory
-        await regionFileStream.ReadAsync(fileCache.Memory);
-        regionFileStream.Seek(0, SeekOrigin.Begin);
-
-        locationTable = new RegionFileHeaderTable(fileCache.Memory.Slice(0, tableSize));
-        timestampTable = new RegionFileHeaderTable(fileCache.Memory.Slice(tableSize, tableSize));
-
-        // Determine end of all allocations/next available allocation
-        nextAvailableOffset = 8192;
-        for (int x = 0; x < cubicRegionSize; x++)
+        for (var index = 0; index < HeaderTableSize; index++)
         {
-            for (int z = 0; z < cubicRegionSize; z++)
-            {
-                var tableIndex = GetChunkTableIndex(new Vector(x, 0, z));
-                var (offset, size) = locationTable.GetOffsetSizeAtIndex(tableIndex);
-                if (size == 0) { continue; }
-                if (offset + size > nextAvailableOffset)
-                {
-                    nextAvailableOffset = offset + size;
-                }
-            }
+            using var num = new RentedArray<byte>(4);
+
+            await this.regionFileStream.ReadAsync(num);
+
+            this.Timestamps[index] = BinaryPrimitives.ReadInt32BigEndian(num);
         }
+
+        await this.regionFileStream.ReadAsync(this.chunkCache.Memory);
+
         return true;
     }
 
-    public async Task FlushToDiskAsync()
+    public async Task SetChunkAsync(int x, int z, byte[] bytes)
     {
-        regionFileStream.Seek(0, SeekOrigin.Begin);
-        await regionFileStream.WriteAsync(fileCache.Memory);
-        await regionFileStream.FlushAsync();
-        regionFileStream.Seek(0, SeekOrigin.Begin);
-    }
+        var tableIndex = this.GetChunkTableIndex(x, z);
 
-    public DateTimeOffset GetChunkTimestamp(Vector relativeChunkLocation) => DateTimeOffset.FromUnixTimeSeconds(timestampTable.GetTimestampAtIndex(GetChunkTableIndex(relativeChunkLocation)));
+        var (offset, size) = this.GetLocation(tableIndex);
 
-    public ReadOnlyMemory<byte> GetChunkCompressedBytes(Vector relativeChunkLocation)
-    {
-        if (locationTable is null || timestampTable is null)
+        if (offset == 0 && size == 0)
         {
-            return ReadOnlyMemory<byte>.Empty;
-        }
+            await this.WriteNewChunkAsync(bytes, tableIndex);
 
-        int chunkIndex = GetChunkTableIndex(relativeChunkLocation);
-        (int offset, int size) = locationTable.GetOffsetSizeAtIndex(chunkIndex);
-        if (size == 0)
-        {
-            return ReadOnlyMemory<byte>.Empty;
-        }
-
-        ReadOnlyMemory<byte> memory = fileCache.Memory.Slice(offset, size);
-        return ChunkAllocation.GetChunkBytes(memory);
-    }
-
-    public void SetChunkCompressedBytes(Vector relativeChunkLocation, byte[] compressedNbtBytes)
-    {
-        // Sanity check
-        if (locationTable is null || timestampTable is null)
-        {
             return;
         }
-        var tableIndex = GetChunkTableIndex(relativeChunkLocation);
-        var (currentOffset, currentSize) = locationTable.GetOffsetSizeAtIndex(tableIndex);
-        var newSize = compressedNbtBytes.Length;
-        Memory<byte> memAllocation;
-        if (newSize <= currentSize && currentSize > 0)
-        {
-            // New chunk will fit in place of the old one.
-            memAllocation = GetAllocation(tableIndex);
-        }
-        else
-        {
-            memAllocation = GetNewAllocation(newSize, tableIndex);
-        }
 
-        var chunkAllocation = new ChunkAllocation(memAllocation);
-        chunkAllocation.SetChunkBytes(compressedNbtBytes);
+        this.SetLocation(tableIndex, offset, bytes.Length);
 
-        var timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
-        timestampTable.SetTimestampAtIndex(tableIndex, timestamp);
+        await this.WriteHeadersAsync();
+
+        this.regionFileStream.Position = offset;
+
+        await this.WriteChunkHeaderAsync(bytes.Length + 1, 0x02);
+
+        await this.regionFileStream.WriteAsync(bytes);
+
+        this.Pad();
+
+        await this.UpdateChunkCache();
     }
 
-    private int GetChunkTableIndex(Vector relativeChunkLoc) => ((relativeChunkLoc.X % cubicRegionSize) + (relativeChunkLoc.Z % cubicRegionSize * cubicRegionSize)) * 4;
-
-    private async Task InitializeNewFileAsync()
+    public ReadOnlyMemory<byte> GetChunkCompressedBytes(int x, int z)
     {
-        using FileStream fs = new FileStream(filePath, FileMode.CreateNew);
-        var initArray = new Memory<byte>(new byte[tableSize * 2]);
-        await fs.WriteAsync(initArray);
-        fs.Seek(0, SeekOrigin.Begin);
+        var tableIndex = this.GetChunkTableIndex(x, z);
+
+        var (offset, size) = this.GetLocation(tableIndex);
+
+        if (offset == 0 && size == 0)
+            return ReadOnlyMemory<byte>.Empty;
+
+        var chunk = this.chunkCache.Memory[(offset - (SectionSize * 2))..];
+
+        var length = BinaryPrimitives.ReadInt32BigEndian(chunk.Span[..4]);
+
+        var compression = chunk.Span[4];//We'll probably make use of this eventually
+
+        if (length > size)
+            throw new InvalidOperationException($"{length} > {size}");
+
+        return chunk.Slice(5, length - 1);//Compression is included with the length
     }
 
-    private Memory<byte> GetAllocation(int tableIndex)
+    public async Task FlushAsync() =>
+       await this.regionFileStream.FlushAsync();
+
+    private async Task WriteNewChunkAsync(byte[] bytes, int tableIndex)
     {
-        var (offset, size) = locationTable.GetOffsetSizeAtIndex(tableIndex);
-        return fileCache.Memory.Slice(offset, size);
+        var size = this.CalculateChunkSize(bytes.Length);
+
+        var offset = this.regionFileStream.Length > 0 ? (int)this.regionFileStream.Length : SectionSize * 2;
+
+        this.SetLocation(tableIndex, offset / SectionSize, size);
+        this.SetTimestamp(tableIndex, (int)DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+
+        await this.WriteHeadersAsync();
+
+        this.regionFileStream.Position = offset;
+
+        await this.WriteChunkHeaderAsync(bytes.Length + 1, 0x02);
+
+        await this.regionFileStream.WriteAsync(bytes);
+
+        this.Pad();
+
+        await this.UpdateChunkCache();
     }
 
-    private Memory<byte> GetNewAllocation(int payloadSize, int tableIndex)
+    private async Task UpdateChunkCache()
     {
-        lock (this)
-        {
-            var allocationSize = ChunkAllocation.GetAllocationSize(payloadSize);
-            var assignedOffset = nextAvailableOffset;
-            Interlocked.Add(ref nextAvailableOffset, allocationSize);
-            locationTable.SetOffsetSizeAtIndex(tableIndex, assignedOffset, allocationSize);
-            return fileCache.Memory.Slice(assignedOffset, allocationSize);
-        }
+        this.chunkCache?.Dispose();
+
+        this.chunkCache = MemoryPool<byte>.Shared.Rent((int)this.regionFileStream.Length - SectionSize * 2);
+
+        this.regionFileStream.Position = SectionSize * 2;
+
+        await this.regionFileStream.ReadAsync(this.chunkCache.Memory);
     }
 
-    /// <summary>
-    /// Chunks are stored in 4k byte allocations.
-    /// Chunk allocations have a 5 byte header.
-    /// Then the rest of the payload is compressed nbt.
-    /// The header is 4 bytes of length in bytes, and one
-    /// byte for compression scheme. 
-    /// </summary>
-    private class ChunkAllocation
+    private void Pad()
     {
-        private readonly Memory<byte> memAllocation;
-        private readonly byte compression;
-        private int blobSize = 0;
+        var missing = this.regionFileStream.Length % SectionSize;
 
-        public ChunkAllocation(Memory<byte> memAllocation, byte compression = 0x1)
-        {
-            this.memAllocation = memAllocation;
-            this.compression = compression;
-        }
-
-        public static int GetAllocationSize(double payloadSize)
-        {
-            // Add the bytes for the header.
-            return (int)Math.Ceiling((payloadSize + 5) / 4096.0) << 12;
-        }
-
-        public void SetChunkBytes(byte[] compressedNbtBytes)
-        {
-            blobSize = compressedNbtBytes.Length;
-            var blobSizeBytes = BitConverter.GetBytes(blobSize);
-            blobSizeBytes.CopyTo(memAllocation.Slice(0, 4).Span);
-            memAllocation.Span[4] = compression;
-            compressedNbtBytes.CopyTo(memAllocation.Slice(5, blobSize));
-        }
-
-        public static ReadOnlyMemory<byte> GetChunkBytes(ReadOnlyMemory<byte> chunkMemory)
-        {
-            // First 5 bytes are a header.
-            // First 4 are filesize.
-            // Fifth is compression scheme. We're always going to use gzip and probably just ignore this.
-            var filesize = BitConverter.ToInt32(chunkMemory.Span[0..4]);
-            // var compression = (int)chunkBytes.Span[4];
-            return chunkMemory.Slice(5, filesize);
-        }
+        if (missing > 0)
+            this.regionFileStream.SetLength(this.regionFileStream.Length + SectionSize - missing);
     }
 
-    private class RegionFileHeaderTable
+    private async Task WriteChunkHeaderAsync(int length, byte compression)
     {
-        private readonly Memory<byte> tableBytes;
+        using var bytes = new RentedArray<byte>(4);
 
-        public RegionFileHeaderTable(Memory<byte> tblBytes)
+        BinaryPrimitives.WriteInt32BigEndian(bytes.Span, length + 1);//Write length and include compression byte
+
+        await this.regionFileStream.WriteAsync(bytes);
+
+        this.regionFileStream.WriteByte(compression);// Write compression
+    }
+
+    private void SetTimestamp(int tableIndex, int time) =>
+        this.Timestamps[tableIndex] = time;
+
+    private void SetLocation(int tableIndex, int offset, int size) =>
+        this.Locations[tableIndex] = (offset << 8) | (size & 0xFF);
+
+    private int CalculateChunkSize(long length) =>
+        (int)Math.Ceiling(length / (double)SectionSize);
+
+    private int GetChunkTableIndex(int x, int z) =>
+        (x & this.op) + (z & this.op) * this.cubicRegionSize;
+
+    private (int offset, int size) GetLocation(int tableIndex)
+    {
+        var sector = this.Locations[tableIndex];
+
+        var offset = sector >> 8;
+        var size = sector & 0xFF;
+
+        return (offset * SectionSize, size * SectionSize);
+    }
+
+    private async Task WriteHeadersAsync()
+    {
+        this.regionFileStream.Position = 0;
+
+        await using var ms = new MemoryStream();
+
+        for (var index = 0; index < HeaderTableSize; index++)
         {
-            tableBytes = tblBytes;
+            using var mem = new RentedArray<byte>(4);
+
+            BinaryPrimitives.WriteInt32BigEndian(mem.Span, this.Locations[index]);
+
+            await ms.WriteAsync(mem);
         }
 
-        public (int offset, int size) GetOffsetSizeAtIndex(int tableIndex)
+        for (var index = 0; index < HeaderTableSize; index++)
         {
-            // Fourth byte is size
-            var size = (int)tableBytes.Span[tableIndex + 3];
-            if (size == 0) { return (0, 0); }
-            // First 3 bytes are offset
-            Span<byte> bytes = new byte[4];
-            tableBytes.Slice(tableIndex, 3).Span.CopyTo(bytes);
-            var offset = BitConverter.ToInt32(bytes);
-            return (offset << 12, size << 12);
+            using var mem = new RentedArray<byte>(4);
+
+            BinaryPrimitives.WriteInt32BigEndian(mem.Span, this.Timestamps[index]);
+
+            await ms.WriteAsync(mem);
         }
 
-        public void SetOffsetSizeAtIndex(int tableIndex, int offset, int size)
-        {
-            byte[] offsetBytes = BitConverter.GetBytes(offset >> 12);
-            offsetBytes = offsetBytes.Take(3).ToArray();
-            offsetBytes.CopyTo(tableBytes.Slice(tableIndex, 3).Span);
-            tableBytes.Span[tableIndex + 3] = (byte)(size >> 12);
-        }
+        ms.Position = 0;
 
-        public long GetTimestampAtIndex(int index) => (long)BitConverter.ToUInt64(tableBytes.Slice(index, 4).Span);
-
-        public void SetTimestampAtIndex(int index, long timestamp) => BitConverter.GetBytes(timestamp).Take(4).ToArray().CopyTo(tableBytes.Slice(index, 4).Span);
+        await ms.CopyToAsync(this.regionFileStream);
     }
 
     #region IDisposable
-    protected async virtual Task DisposeAsync(bool disposing)
+    private async Task DisposeAsync(bool disposing)
     {
-        if (!disposedValue)
+        if (!this.disposed)
         {
             if (disposing)
             {
-                await FlushToDiskAsync();
+                await this.regionFileStream.FlushAsync();
+                await this.regionFileStream.DisposeAsync();
+
+                this.chunkCache.Dispose();
             }
 
             // TODO: free unmanaged resources (unmanaged objects) and override finalizer
             // TODO: set large fields to null
-            disposedValue = true;
+            this.disposed = true;
         }
     }
 
