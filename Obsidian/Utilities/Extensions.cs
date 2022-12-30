75026452589adb3e8c3d0d3eb3d3f54584f6f6a7
Tides <losttides@gmail.com>
2022-12-30 02:57:27 -0500

    Some quick additions
    Microsoft apparently can't have a working one

--- Obsidian/Utilities/Extensions.cs
+++ Obsidian/Utilities/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using Obsidian.Entities;
+using Obsidian.Net;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
@@ -53,6 +54,24 @@ public static partial class Extensions
 
 
     public static ParameterExpression[] GetParamExpressions(this Type[] types) => types.Select((t, i) => Expression.Parameter(t, $"param{i}")).ToArray();
+
+    public static void Write(this SkinProperty skinProperty, MinecraftStream stream)
+    {
+        stream.WriteString(skinProperty.Name);
+        stream.WriteString(skinProperty.Value);
+        stream.WriteBoolean(skinProperty.Signature != null);
+        if (skinProperty.Signature is string data)
+            stream.WriteString(data);
+    }
+
+    public static async Task WriteAsync(this SkinProperty skinProperty, MinecraftStream stream)
+    {
+        await stream.WriteStringAsync(skinProperty.Name);
+        await stream.WriteStringAsync(skinProperty.Value);
+        await stream.WriteBooleanAsync(skinProperty.Signature != null);
+        if (skinProperty.Signature is string data)
+            await stream.WriteStringAsync(data);
+    }
 
     internal static bool IsNonLiving(this EntityType type) => nonLiving.Contains(type);
 
