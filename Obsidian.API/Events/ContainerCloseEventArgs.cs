0f68c6d3a8e2b04a51224c814b6059a2d2814815
Tides <losttides@gmail.com>
2023-04-29 03:14:03 -0400

    Look

--- Obsidian.API/Events/ContainerCloseEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Obsidian.API.Events;
-public sealed class ContainerCloseEventArgs : ContainerEventArgs, ICancellable
-{
-    public bool Cancel { get; set; }
-
-    internal ContainerCloseEventArgs(IPlayer player) : base(player)
-    {
-    }
-}
---
+++ Obsidian.API/Events/ContainerClosedEventArgs.cs
@@ -0,0 +1,9 @@
+﻿namespace Obsidian.API.Events;
+public sealed class ContainerClosedEventArgs : ContainerEventArgs, ICancellable
+{
+    public bool Cancel { get; set; }
+
+    internal ContainerClosedEventArgs(IPlayer player) : base(player)
+    {
+    }
+}
