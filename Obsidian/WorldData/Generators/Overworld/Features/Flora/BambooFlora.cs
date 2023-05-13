15048bf283e6cfa0dab4596e1c1f09b54522e3bf
Jon Pro <jonpro03@gmail.com>
2023-05-12 22:14:05 -0500

    Add Bamboo Jungle and Sunflower Plains biome decorations

---
+++ Obsidian/WorldData/Generators/Overworld/Features/Flora/BambooFlora.cs
@@ -0,0 +1,12 @@
+﻿using Obsidian.API.BlockStates.Builders;
+
+namespace Obsidian.WorldData.Generators.Overworld.Features.Flora;
+
+public class BambooFlora : BaseTallFlora
+{
+    public BambooFlora(GenHelper helper, Chunk chunk) : 
+        base(helper, chunk, Material.Bamboo, 15)
+    {
+
+    }
+}
---
+++ Obsidian/WorldData/Generators/Overworld/Features/Flora/PumpkinFlora.cs
@@ -0,0 +1,9 @@
+﻿namespace Obsidian.WorldData.Generators.Overworld.Features.Flora;
+
+public class PumpkinFlora : BaseFlora
+{
+    public PumpkinFlora(GenHelper helper, Chunk chunk) : base(helper, chunk, Material.Pumpkin)
+    {
+
+    }
+}
