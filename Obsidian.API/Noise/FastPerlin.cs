00c8c1e7b1c13d77ecb4f626f487c93dd02f8fa7
Jon Pro <jonpro03@gmail.com>
2023-11-17 13:58:10 -0600

    Working now

--- Obsidian.API/Noise/FastPerlin.cs
+++ Obsidian.API/Noise/FastPerlin.cs
@@ -1,18 +1,38 @@
 ﻿using FastNoiseOO;
 using FastNoiseOO.Generators;
+using SharpNoise;
 using SharpNoise.Modules;
 
 namespace Obsidian.API.Noise;
 
 public class FastPerlin : SharpNoise.Modules.Perlin
 {
+    private readonly FastNoiseOO.Generators.Perlin prln;
+    public FastPerlin()
+    {
+        prln = new FastNoiseOO.Generators.Perlin();
+    }
+
     public override double GetValue(double x, double y, double z)
     {
-        var prln = new FastNoiseOO.Generators.Perlin();
-        //prln.Set("Frequency", (float)Frequency);
-        //prln.Set("Lacunarity", (float)Lacunarity);
-        //prln.Set("OctaveCount", (float)OctaveCount);
-        //prln.Set("Persistence", (float)Persistence);
-        return prln.GenSingle3D((float)x, (float)y, (float)z, Seed);
+        double num = 0.0;
+        double num2 = 1.0;
+        x *= Frequency;
+        y *= Frequency;
+        z *= Frequency;
+        for (int i = 0; i < OctaveCount; i++)
+        {
+            int seed = (Seed + i) & 0x7FFFFFFF;
+            double num3 = prln.GenSingle3D((float)x, (float)y, (float)z, seed);
+            num += num3 * num2;
+            x *= Lacunarity;
+            y *= Lacunarity;
+            z *= Lacunarity;
+            num2 *= Persistence;
+        }
+
+        return num;
+
+        //return base.GetValue(x, y, z);
     }
 }
