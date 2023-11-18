93e007549c93622e7766192dd0990d500eb0f9f6
Jon Pro <jonpro03@gmail.com>
2023-11-18 09:38:19 -0600

    a bit of cleanup

--- Obsidian.API/Noise/FastPerlin.cs
+++ Obsidian.API/Noise/FastPerlin.cs
@@ -11,7 +11,7 @@ public class FastPerlin : SharpNoise.Modules.Perlin
 {
     private readonly FastNoiseOO.Generators.Perlin prln;
 
-    private ConcurrentDictionary<(int chunkX, int chunkZ), ConcurrentDictionary<double, float[]>> storage2 = new();
+    private ConcurrentDictionary<(int chunkX, int chunkZ), ConcurrentDictionary<double, float[]>> storage = new();
 
     public FastPerlin()
     {
@@ -20,28 +20,23 @@ public class FastPerlin : SharpNoise.Modules.Perlin
 
     public void Cleanup(int chunkX, int chunkZ)
     {
-        try
-        {
-            storage2.TryRemove((chunkX, chunkZ), out var _);
-        } catch(Exception e)  {
-            Console.WriteLine("");
-        }
+        storage.TryRemove((chunkX, chunkZ), out var _);
     }
 
     public override double GetValue(double x, double y, double z)
     {
         var (chunkX, chunkZ) = ((int)x >> 4, (int)z >> 4);
         var (startX, startZ) = (chunkX << 4, chunkZ << 4);
-        if (storage2.TryGetValue((chunkX, chunkZ), out var yDict))
+        if (storage.TryGetValue((chunkX, chunkZ), out var yDict))
         {
             if (yDict.TryGetValue(y, out var arr))
             {
                 return arr[(((int)z - startZ) << 4) + ((int)x - startX)];
             }
-        } 
+        }
         else
         {
-            storage2[(chunkX, chunkZ)] = new();
+            storage[(chunkX, chunkZ)] = new();
         }
 
         // Get all of the X/Z values for this Y
@@ -53,36 +48,17 @@ public class FastPerlin : SharpNoise.Modules.Perlin
         {
             int seed = (Seed + o) & 0x7FFFFFFF;
             var r = prln.GenUniformGrid2D(startX, startZ, 16, 16, (float)Frequency * lacunarityFactor, seed, out var minMax);
-            for (int i = 0; i < 16*16; i++)
+            for (int i = 0; i < 16 * 16; i++)
             {
                 resultArray[i] += r[i] * persistenceFactor;
             }
             lacunarityFactor *= (float)Lacunarity;
             persistenceFactor *= (float)Persistence;
         }
-        storage2[(chunkX, chunkZ)][y] = resultArray;
+        storage[(chunkX, chunkZ)][y] = resultArray;
 
         return resultArray[(((int)z - startZ) << 4) + ((int)x - startX)];
 
-
-        /*        double num = 0.0;
-                double num2 = 1.0;
-                x *= Frequency;
-                y *= Frequency;
-                z *= Frequency;
-                for (int i = 0; i < OctaveCount; i++)
-                {
-                    int seed = (Seed + i) & 0x7FFFFFFF;
-                    double num3 = prln.GenSingle3D((float)x, (float)y, (float)z, seed);
-                    num += num3 * num2;
-                    x *= Lacunarity;
-                    y *= Lacunarity;
-                    z *= Lacunarity;
-                    num2 *= Persistence;
-                }
-
-                return num;
-        */
         //return base.GetValue(x, y, z);
     }
 }
