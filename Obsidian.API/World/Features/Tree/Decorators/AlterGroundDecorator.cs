198c6bb00831da520859283877a30c855a9f5972
Tides <losttides@gmail.com>
2024-02-13 18:40:28 -0500

    Tree decorators

---
+++ Obsidian.API/World/Features/Tree/Decorators/AlterGroundDecorator.cs
@@ -0,0 +1,7 @@
+﻿namespace Obsidian.API.World.Features.Tree.Decorators;
+public sealed class AlterGroundDecorator : DecoratorBase
+{
+    public override string Type => "alter_ground";
+
+    public required IBlockStateProvider Provider { get; set; }
+}
---
+++ Obsidian.API/World/Features/Tree/Decorators/AttachToLeavesDecorator.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Obsidian.API.World.Features.Tree.Decorators;
+public sealed class AttachToLeavesDecorator : DecoratorBase
+{
+    public override string Type => "attached_to_leaves";
+
+    [Range(0, 16)]
+    public required int ExclusionRadiusXZ { get; set; }
+
+    [Range(0, 16)]
+    public required int ExclusionRadiusY { get; set; }
+
+    [Range(0, 16)]
+    public required int RequiredEmptyBlocks { get; set; }
+
+    public required IBlockStateProvider BlockProvider { get; set; }
+
+    /// <summary>
+    /// Directions to generate. 
+    /// </summary>
+    /// <remarks>
+    /// Cannot be empty. 
+    /// Must be up, down, north, south , west, or east.
+    /// </remarks>
+    public List<string> Directions { get; } = [];
+}
