89aee459aa5a9ee42b69e3e1173adfd6a7f8376a
Tides <losttides@gmail.com>
2024-02-13 18:48:25 -0500

    Root placers

---
+++ Obsidian.API/World/Features/Tree/Placers/Root/MangroveRootPlacer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Obsidian.API.World.Features.Tree.Placers.Root;
+public sealed class MangroveRootPlacer : RootPlacer
+{
+    public override string Type => "mangrove_root_placer";
+
+    public required MangrovePlacement MangroveRootPlacement { get; set; }
+
+    public sealed class MangrovePlacement
+    {
+        [Range(1, 12)]
+        public required int MaxRootWidth { get; set; }
+
+        [Range(1, 12)]
+        public required int MaxRootLength { get; set; }
+
+        [Range(0.0, 1.0)]
+        public required float RandomSkewChance { get; set; }
+
+        public List<string> CanGrowThrough { get; } = [];
+        public List<string> MuddyRootsIn { get; } = [];
+
+        public required IBlockStateProvider MuddyRootsProvider { get; set; }
+    }
+}
