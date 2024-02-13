b8b97c00b717b0046608161d895801d92071e206
Tides <losttides@gmail.com>
2024-02-13 16:52:05 -0500

    TreeSizeBase

---
+++ Obsidian.API/World/Features/Tree/ThreeLayersFeatureSize.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Obsidian.API.World.Features.Tree;
+public class ThreeLayersFeatureSize : TwoLayersFeatureSize
+{
+    public override string Type => "three_layers_feature_size";
+
+    [Range(0, 80)]
+    public override int Limit { get; set; } = 1;
+
+    [Range(0, 80)]
+    public int UpperLimit { get; set; } = 1;
+
+    [Range(0, 16)]
+    public int MiddleSize { get; set; } = 1;
+}
---
+++ Obsidian.API/World/Features/Tree/TwoLayersFeatureSize.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Obsidian.API.World.Features.Tree;
+public class TwoLayersFeatureSize : TreeSizeBase
+{
+    public override string Type => "two_layers_feature_size";
+
+    [Range(0, 81)]
+    public virtual int Limit { get; set; } = 1;
+
+    [Range(0, 16)]
+    public virtual int LowerSize { get; set; }
+
+    [Range(0, 16)]
+    public virtual int UpperSize { get; set; } = 1;
+}
