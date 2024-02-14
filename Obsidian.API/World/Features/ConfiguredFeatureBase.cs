308930ad4172e7df6bb78f4e157601faaac79f6c
Tides <losttides@gmail.com>
2024-02-13 19:29:47 -0500

    IFeature

---
+++ Obsidian.API/World/Features/ConfiguredFeatureBase.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Obsidian.API.World.Features;
+public abstract class ConfiguredFeatureBase : IWorldFeature
+{
+    public abstract string Type { get; }
+    public virtual bool CanPlace(FeatureContext context) => true;
+    public abstract ValueTask Place(FeatureContext context);
+}
---
+++ Obsidian.API/World/Features/Tree/BaseTreeFeature.cs
@@ -0,0 +1,31 @@
+﻿using Obsidian.API.World.Features.Tree.Decorators;
+using Obsidian.API.World.Features.Tree.Placers;
+using Obsidian.API.World.Features.Tree.Placers.Foliage;
+using Obsidian.API.World.Features.Tree.Placers.Trunk;
+
+namespace Obsidian.API.World.Features.Tree;
+public abstract class BaseTreeFeature : ConfiguredFeatureBase
+{
+    public override string Type => "minecraft:tree";
+    public virtual bool IgnoreVines { get; set; }
+    public virtual bool ForceDirt { get; set; }
+
+    public abstract TreeSizeBase MinimumSize { get; }
+
+    public virtual RootPlacer? RootPlacer { get; }
+
+    public abstract TrunkPlacer TrunkPlacer { get; }
+    public abstract FoliagePlacer FoliagePlacer { get; }
+
+    public List<DecoratorBase> Decorators { get; } = [];
+
+    public override abstract ValueTask Place(FeatureContext context);
+
+    public abstract void ConfigureProviders(Providers providers);
+    public sealed class Providers
+    {
+        public IBlockStateProvider DirtProvider { get; set; } = default!;
+        public IBlockStateProvider TrunkProvider { get; set; } = default!;
+        public IBlockStateProvider FoliageProvider { get; set; } = default!;
+    }
+}
