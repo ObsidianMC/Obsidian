98a74e82ff7cdf0732c90ac9a31268dd7f2f3bbe
Tides <losttides@gmail.com>
2024-02-17 19:19:53 -0500

    Update biome element

--- Obsidian.API/Registry/Codecs/Biomes/BiomeElement.cs
+++ Obsidian.API/Registry/Codecs/Biomes/BiomeElement.cs
@@ -1,4 +1,7 @@
-﻿namespace Obsidian.API.Registry.Codecs.Biomes;
+﻿using Obsidian.API.Utilities.Json.Converters;
+using System.Text.Json.Serialization;
+
+namespace Obsidian.API.Registry.Codecs.Biomes;
 
 public sealed record class BiomeElement
 {
@@ -6,6 +9,12 @@ public sealed record class BiomeElement
 
     public required bool HasPrecipitation { get; set; }
 
+    public string[][] Features { get; set; } = [];
+
+    [JsonConverter(typeof(SpecialDictionaryConverter))]
+    public Dictionary<string, string[]> Carvers { get; set; } = default!;
+    public Dictionary<string, SpawnerMob[]> Spawners { get; set; } = default!;
+    public Dictionary<string, object> SpawnCosts { get; set; } = default!;
 
     public float Depth { get; set; }
     public float Temperature { get; set; }
@@ -16,4 +25,18 @@ public sealed record class BiomeElement
     public string? TemperatureModifier { get; set; }
 
     public bool PlayerSpawnFriendly { get; set; }
+}
+
+public sealed class SpawnerMob
+{
+    public required string Type { get; init; }
+
+    [JsonPropertyName("maxCount")]
+    public required int MaxCount { get; init; }
+
+    [JsonPropertyName("minCount")]
+    public required int MinCount { get; init; }
+
+    [JsonPropertyName("weight")]
+    public required int Weight { get; init; }
 }
