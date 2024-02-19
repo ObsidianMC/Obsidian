442bad8ae6ea8387c3e84b8dc6dfb6821475af7c
Tides <losttides@gmail.com>
2024-02-18 21:26:13 -0500

    Update Obsidian.SourceGenerators.csproj

--- Obsidian.SourceGenerators/Registry/NameHelper.cs
+++ Obsidian.SourceGenerators/Registry/NameHelper.cs
@@ -9,26 +9,30 @@ internal static class NameHelper
         return namespacedName.Substring(namespacedName.IndexOf(":") + 1);
     }
 
-    public static string ToPascalCase(this string snakeCase)
+    public static string ToPascalCase(this string value)
     {
         // Alternative implementation:
         // var textInfo = System.Globalization.CultureInfo.CurrentCulture.TextInfo;
         // return string.Join("", snakeCase.Split('_').Select(s => textInfo.ToTitleCase(s)));
 
+        //Assume its camelCase
+        if (!value.Contains("_"))
+            return new string([char.ToUpper(value[0]), .. value.Substring(1)]);
+
         int spaceCount = 0;
-        for (int i = 0; i < snakeCase.Length; i++)
+        for (int i = 0; i < value.Length; i++)
         {
-            if (!char.IsLetterOrDigit(snakeCase[i]))
+            if (!char.IsLetterOrDigit(value[i]))
                 spaceCount++;
         }
 
-        var result = new char[snakeCase.Length - spaceCount];
+        var result = new char[value.Length - spaceCount];
 
         int targetIndex = 0;
         bool wordStart = true;
-        for (int i = 0; i < snakeCase.Length; i++)
+        for (int i = 0; i < value.Length; i++)
         {
-            char c = snakeCase[i];
+            char c = value[i];
             if (char.IsLetterOrDigit(c))
             {
                 result[targetIndex++] = wordStart ? char.ToUpper(c) : char.ToLower(c);
