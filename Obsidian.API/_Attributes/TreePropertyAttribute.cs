ae4e3e56b3ac036de8c49b219af03e9fceb4f65d
Tides <losttides@gmail.com>
2024-02-20 15:09:33 -0500

    Change attribute for property discovery

--- Obsidian.API/_Attributes/TreePropertyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Obsidian.API;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-public sealed class TreePropertyAttribute : Attribute
-{
-    public string ResourceLocation { get; init; }
-
-    public TreePropertyAttribute(string resourceLocation) => this.ResourceLocation = resourceLocation;
-}
