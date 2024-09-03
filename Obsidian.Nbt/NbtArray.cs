8cd3ebc41778fff0df18dfbe6de6600cb60b11b0
Tides <losttides@gmail.com>
2024-09-03 01:13:13 -0400

    Further cleanup NbtArray

--- Obsidian.Nbt/NbtArray.cs
+++ Obsidian.Nbt/NbtArray.cs
@@ -11,87 +11,44 @@ public sealed class NbtArray<T> : INbtTag, IEnumerable, ICollection where T : st
 
     public NbtTagType Type { get; private set; }
 
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
-    public INbtTag Parent { get; set; }
+    public INbtTag? Parent { get; set; }
 
-    public bool IsSynchronized => this.array.IsSynchronized;
+    public bool IsSynchronized => false;
 
-    public object SyncRoot => this.array.SyncRoot;
+    public object SyncRoot => this.array;
 
     public T this[int index] { get => this.array[index]; set => this.array[index] = value; }
 
-    public NbtArray(string name, int length)
-    {
-        (this.Name, this.array) = (name, new T[length]);
-
-        this.SetType();
-    }
-
-    public NbtArray(string name, IEnumerable<T> array)
-    {
-        (this.Name, this.array) = (name, array.ToArray());
-
-        this.SetType();
-    }
-
-    public NbtArray(string name, T[] array)
+    public NbtArray(string? name, T[] array)
     {
         (this.Name, this.array) = (name, array);
 
-        this.SetType();
+        this.Type = this.DetermingType();
     }
 
     public void CopyTo(Array array, int index) => this.array.CopyTo(array, index);
 
     public IEnumerator GetEnumerator() => this.array.GetEnumerator();
 
-    public bool Contains(T item) => this.array.Contains(item);
+    public bool Contains(T item) => Array.IndexOf(this.array, item) >= 0;
 
     public T[] GetArray() => this.array;
 
-    public override string ToString()
-    {
-        switch (this.Type)
-        {
-            case NbtTagType.ByteArray:
-            case NbtTagType.IntArray:
-            case NbtTagType.LongArray:
-                return $"TAG_{this.Type}('{this.Name}'): {this.Count} Values";
-            default:
-                throw new InvalidOperationException();
-        }
-    }
+    public override string ToString() => $"TAG_{this.Type}('{this.Name}'): {this.Count} Values";
 
     public string PrettyString(int depth = 2, int addBraceDepth = 1)
     {
-        switch (this.Type)
-        {
-            case NbtTagType.ByteArray:
-            case NbtTagType.IntArray:
-            case NbtTagType.LongArray:
-                {
-                    var name = $"TAG_{this.Type}('{this.Name}'): {this.Count} Values";
-                    return name.PadLeft(name.Length + depth);
-                }
-            default:
-                throw new InvalidOperationException();
-        }
+        var name = $"TAG_{this.Type}('{this.Name}'): {this.Count} Values";
+        return name.PadLeft(name.Length + depth);
     }
 
-    private void SetType()
+    private NbtTagType DetermingType() => typeof(T) switch
     {
-        if (typeof(T) == typeof(int))
-        {
-            this.Type = NbtTagType.IntArray;
-        }
-        else if (typeof(T) == typeof(long))
-        {
-            this.Type = NbtTagType.LongArray;
-        }
-        else if (typeof(T) == typeof(byte))
-        {
-            this.Type = NbtTagType.ByteArray;
-        }
-    }
+        Type t when t == typeof(int) => NbtTagType.IntArray,
+        Type t when t == typeof(long) => NbtTagType.LongArray,
+        Type t when t == typeof(byte) => NbtTagType.ByteArray,
+        _ => throw new NotSupportedException($"Type {nameof(T)} is not supported.")
+    };
 }
