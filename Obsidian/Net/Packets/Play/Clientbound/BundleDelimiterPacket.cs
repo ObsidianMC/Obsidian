1ebb627ef6de561cbad35724d6405f00dc705c6c
Tides <losttides@gmail.com>
2024-09-13 02:58:16 -0400

    Update BundleDelimiterPacket to take a list of packets

--- Obsidian/Net/Packets/Play/Clientbound/BundleDelimiterPacket.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Obsidian.Net.Packets.Play.Clientbound;
-public sealed partial class BundleDelimiterPacket : IClientboundPacket
-{
-    public int Id => 0x00;
-
-    public void Serialize(MinecraftStream stream) { }
-}
