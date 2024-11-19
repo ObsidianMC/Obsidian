2f31eee9ca5e7ffdf5592045b36b9508ca5c789d
Tides <losttides@gmail.com>
2024-11-19 03:47:26 -0600

    Phase out serialization methods generator

--- Obsidian.SourceGenerators/Packets/MethodBuildingContext.cs
+++ Obsidian.SourceGenerators/Packets/MethodBuildingContext.cs
@@ -1,25 +1,13 @@
 ﻿namespace Obsidian.SourceGenerators.Packets;
 
-internal sealed class MethodBuildingContext
+internal sealed class MethodBuildingContext(string streamName, string dataName, Property property, IReadOnlyList<Property> allProperties, CodeBuilder codeBuilder, Method method, MethodsRegistry methodsRegistry, SourceProductionContext context)
 {
-    public string StreamName { get; }
-    public string DataName { get; }
-    public Method Method { get; }
-    public CodeBuilder CodeBuilder { get; }
-    public MethodsRegistry MethodsRegistry { get; }
-    public GeneratorExecutionContext GeneratorContext { get; }
-    public Property Property { get; }
-    public IReadOnlyList<Property> AllProperties { get; }
-
-    public MethodBuildingContext(string streamName, string dataName, Property property, IReadOnlyList<Property> allProperties, CodeBuilder codeBuilder, Method method, MethodsRegistry methodsRegistry, GeneratorExecutionContext context)
-    {
-        StreamName = streamName;
-        DataName = dataName;
-        Property = property;
-        AllProperties = allProperties;
-        CodeBuilder = codeBuilder;
-        Method = method;
-        MethodsRegistry = methodsRegistry;
-        GeneratorContext = context;
-    }
+    public string StreamName { get; } = streamName;
+    public string DataName { get; } = dataName;
+    public Method Method { get; } = method;
+    public CodeBuilder CodeBuilder { get; } = codeBuilder;
+    public MethodsRegistry MethodsRegistry { get; } = methodsRegistry;
+    public SourceProductionContext GeneratorContext { get; } = context;
+    public Property Property { get; } = property;
+    public IReadOnlyList<Property> AllProperties { get; } = allProperties;
 }
