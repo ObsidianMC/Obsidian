a70092e169a7f05057a75745540e6edf956c8e4c
Tides <losttides@gmail.com>
2024-12-12 03:41:20 -0600

    Sorgegen data components enum

---
+++ Obsidian.SourceGenerators/Registry/RegistryAssetsGenerator.DataComponents.cs
@@ -0,0 +1,35 @@
+﻿using Obsidian.SourceGenerators.Packets;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Obsidian.SourceGenerators.Registry;
+public partial class RegistryAssetsGenerator
+{
+    private static void GenerateDataComponentsEnum(string dataComponentsJson, SourceProductionContext ctx)
+    {
+        var components = JsonSerializer.Deserialize<Dictionary<string, Protocol>>(dataComponentsJson)!;
+
+        var builder = new CodeBuilder()
+           .Namespace("Obsidian.API")
+           .Line()
+           .Type("public enum DataComponentType");
+
+        foreach (var component in components.OrderBy(x => x.Value.ProtocolId))
+        {
+            var name = component.Key.RemoveNamespace().ToPascalCase();
+            var protocolId = component.Value.ProtocolId;
+
+            builder.Line($"{name} = {protocolId},");
+        }
+
+        builder.EndScope();
+
+        ctx.AddSource("DataComponentType.g.cs", builder.ToString());
+    }
+
+    private sealed class Protocol
+    {
+        [JsonPropertyName(Vocabulary.ProtocolId)]
+        public int ProtocolId { get; set; }
+    }
+}
--- Obsidian.SourceGenerators/Registry/RegistryAssetsGenerator.cs
+++ Obsidian.SourceGenerators/Registry/RegistryAssetsGenerator.cs
@@ -34,6 +34,7 @@ public sealed partial class RegistryAssetsGenerator : IIncrementalGenerator
             GenerateCodecs(assets, context);
             GenerateMaterials(assets, context);
             GenerateSounds(assets, context);
+            GenerateDataComponentsEnum(output.files.GetJsonFromArray("data_components"), context);
         }
     }
 }
