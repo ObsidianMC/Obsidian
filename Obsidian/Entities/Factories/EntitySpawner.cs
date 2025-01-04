ab9b026034b32e011e5bb891875c4f038f3b37d9
Ryan <ryandejonge@outlook.com>
2025-01-04 08:21:46 +0100

    Add more methods to EntitySpawner

--- Obsidian/Entities/Factories/EntitySpawner.cs
+++ Obsidian/Entities/Factories/EntitySpawner.cs
@@ -12,13 +12,20 @@ internal class EntitySpawner : IEntitySpawner
 {
     private IWorld world;
 
-    private EntityType entityType;
+    private EntityType? entityType = null;
 
     private int entityId = 0;
     private VectorF position = VectorF.Zero;
     private bool isBaby = false;
     private string? customName = null;
     private bool customNameVisible = false;
+    private LivingBitMask livingBitMask;
+    private bool ambientPotionEffect;
+    private int absorbedArrows;
+    private int absorbtionAmount;
+    private int absorbedStingers;
+    private bool burning;
+    private bool glowing;
 
     public EntitySpawner(IWorld world)
     {
@@ -56,6 +63,48 @@ internal class EntitySpawner : IEntitySpawner
         return this;
     }
 
+    public IEntitySpawner WithLivingBitMask(LivingBitMask bitMask)
+    {
+        livingBitMask = bitMask;
+        return this;
+    }
+
+    public IEntitySpawner WithAmbientPotionEffect(bool ambient)
+    {
+        ambientPotionEffect = ambient;
+        return this;
+    }
+
+    public IEntitySpawner WithAbsorbedArrows(int arrows)
+    {
+        absorbedArrows = arrows;
+        return this;
+    }
+
+    public IEntitySpawner WithAbsorbtionAmount(int amount)
+    {
+        absorbtionAmount = amount;
+        return this;
+    }
+
+    public IEntitySpawner WithAbsorbedStingers(int stingers)
+    {
+        absorbedStingers = stingers;
+        return this;
+    }
+
+    public IEntitySpawner IsBurning()
+    {
+        this.burning = true;
+        return this;
+    }
+
+    public IEntitySpawner IsGlowing()
+    {
+        this.glowing = true;
+        return this;
+    }
+
     public IEntity Spawn()
     {
         var packetBroadcaster = (world as World).PacketBroadcaster;
@@ -78,22 +127,25 @@ internal class EntitySpawner : IEntitySpawner
                 PacketBroadcaster = packetBroadcaster,
                 World = world,
             },
-            EntityType.Donkey => new Donkey() 
-            { 
-                PacketBroadcaster = packetBroadcaster, 
-                World = world 
+            EntityType.Donkey => new Donkey()
+            {
+                PacketBroadcaster = packetBroadcaster,
+                World = world
             },
-            EntityType.SkeletonHorse => new SkeletonHorse() 
-            { 
-                PacketBroadcaster = packetBroadcaster, 
-                World = world 
+            EntityType.SkeletonHorse => new SkeletonHorse()
+            {
+                PacketBroadcaster = packetBroadcaster,
+                World = world
             },
-            EntityType.ZombieHorse => new ZombieHorse() 
-            { 
-                PacketBroadcaster = packetBroadcaster, 
-                World = world 
+            EntityType.ZombieHorse => new ZombieHorse()
+            {
+                PacketBroadcaster = packetBroadcaster,
+                World = world
             },
-            _ => entityType.IsNonLiving() ?
+
+            null => throw new InvalidOperationException("Entity type must be set"),
+
+            _ => entityType.Value.IsNonLiving() ?
             new Entity()
             {
                 PacketBroadcaster = packetBroadcaster,
@@ -102,19 +154,33 @@ internal class EntitySpawner : IEntitySpawner
             new Living()
             {
                 PacketBroadcaster = packetBroadcaster,
-                World = world,
+                World = world
             }
         };
 
-        entity.Type = entityType;
+        entity.Type = entityType.Value;
         entity.EntityId = entityId;
         entity.Position = position;
 
-        if(entity is ILiving living && customName != null)
+        if (entity is Living living && customName != null)
         {
             living.CustomName = customName;
             living.CustomNameVisible = customNameVisible;
+
+            living.LivingBitMask = livingBitMask;
+            living.AmbientPotionEffect = ambientPotionEffect;
+            living.AbsorbedArrows = absorbedArrows;
+            living.AbsorbtionAmount = absorbtionAmount;
+            living.AbsorbedStingers = absorbedStingers;
         }
+
+        if (entity is AgeableMob ageable && isBaby)
+        {
+            ageable.IsBaby = isBaby;
+        }
+
+        entity.Burning = burning;
+        entity.Glowing = glowing;
 
         return (world as World).SpawnEntity(entity);
     }
