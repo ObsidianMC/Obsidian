f904d4b5a451ed0384a86a3b7be4df2380f4a645
RoboMico <robomico@outlook.com>
2025-01-31 20:45:11 +0800

    feat: send compressed packets (#393)
    Implemented MinecraftStream.WriteCompressedPacket and enabled compression in Client class. Also rewrote MinecraftStream.WritePacket because how it sent bundled packets was confusing.

--- Obsidian/Net/Packets/ClientboundPacket.cs
+++ Obsidian/Net/Packets/ClientboundPacket.cs
@@ -7,5 +7,9 @@ public abstract class ClientboundPacket : IClientboundPacket
 {
     public abstract int Id { get; }
 
+    /// <summary>
+    /// Writes the data content of the packet to the given stream.
+    /// </summary>
+    /// <param name="writer"></param>
     public virtual void Serialize(INetStreamWriter writer) { }
 }
