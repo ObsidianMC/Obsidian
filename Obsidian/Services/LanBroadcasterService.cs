c2b428da3342d512c9f712afd0bb9bffef1f71e1
Junior Ortiz Cabrera <losttides@gmail.com>
2025-07-16 17:55:30 -0500

    Fix resource disposal and add error handling

--- Obsidian/Services/LanBroadcasterService.cs
+++ Obsidian/Services/LanBroadcasterService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Obsidian.API.Configuration;
 using System.Net.Sockets;
@@ -9,15 +10,17 @@ namespace Obsidian.Services;
 public sealed class LanBroadcasterService : BackgroundService
 {
     private readonly IDisposable optionsChanged;
+    private readonly ILogger<LanBroadcasterService> logger;
     private ServerConfiguration configuration;
 
-    public LanBroadcasterService(IOptionsMonitor<ServerConfiguration> options)
+    public LanBroadcasterService(IOptionsMonitor<ServerConfiguration> options, ILogger<LanBroadcasterService> logger)
     {
         this.configuration = options.CurrentValue;
         this.optionsChanged = options.OnChange((configuration, _) =>
         {
             this.configuration = configuration;
         });
+        this.logger = logger;
     }
 
     protected async override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -25,19 +28,27 @@ public sealed class LanBroadcasterService : BackgroundService
         if (!this.configuration.AllowLan)
             return;
 
-        var udpClient = new UdpClient("224.0.2.60", 4445);
-        var timer = new PeriodicTimer(TimeSpan.FromSeconds(1.5));
+        using var udpClient = new UdpClient("224.0.2.60", 4445);
+        using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1.5));
         string? lastMotd = null;
         byte[] bytes = []; // Cached motd as utf-8 bytes
-        while (await timer.WaitForNextTickAsync(stoppingToken))
-        {
-            if (this.configuration.Motd != lastMotd)
-            {
-                lastMotd = this.configuration.Motd;
-                bytes = Encoding.UTF8.GetBytes($"[MOTD]{this.configuration.Motd.Replace('[', '(').Replace(']', ')')}[/MOTD][AD]{this.configuration.Port}[/AD]");
-            }
 
-            await udpClient.SendAsync(bytes, bytes.Length);
+        try
+        {
+            while (await timer.WaitForNextTickAsync(stoppingToken))
+            {
+                if (this.configuration.Motd != lastMotd)
+                {
+                    lastMotd = this.configuration.Motd;
+                    bytes = Encoding.UTF8.GetBytes($"[MOTD]{this.configuration.Motd.Replace('[', '(').Replace(']', ')')}[/MOTD][AD]{this.configuration.Port}[/AD]");
+                }
+
+                await udpClient.SendAsync(bytes, bytes.Length);
+            }
+        }
+        catch (Exception ex) when (ex is not OperationCanceledException)
+        {
+            this.logger.LogError(ex, "An error occurred while broadcasting LAN information.");
         }
     }
 
