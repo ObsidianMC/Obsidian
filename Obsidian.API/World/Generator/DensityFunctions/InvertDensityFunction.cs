5ca88d1c7e95457a1c9b9ea6e2894a3852664395
Junior Ortiz Cabrera <losttides@gmail.com>
2025-11-07 23:37:55 -0600

    Update all assets

---
+++ Obsidian.API/World/Generator/DensityFunctions/InvertDensityFunction.cs
@@ -0,0 +1,15 @@
+﻿namespace Obsidian.API.World.Generator.DensityFunctions;
+
+[DensityFunction("minecraft:invert")]
+public sealed class InvertDensityFunction : IDensityFunction
+{
+    public double MinValue { get; init; }
+
+    public double MaxValue { get; init; }
+
+    public string Type { get; init; }
+
+    public IDensityFunction Argument { get; init; }
+
+    public double GetValue(double x, double y, double z) => throw new NotImplementedException();
+}
