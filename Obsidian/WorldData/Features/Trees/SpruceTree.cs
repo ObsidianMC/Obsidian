f4f16d181f60e1be35e2626eb08cc1ff18d2fdcc
Jon Pro <jonpro03@gmail.com>
2025-12-01 09:33:51 -0600

    fix: remove old tree generators (#504)

--- Obsidian/WorldData/Features/Trees/SpruceTree.cs
+++
@@ -1,7 +0,0 @@
-﻿using Obsidian.WorldData.Generators;
-
-namespace Obsidian.WorldData.Features.Trees;
-
-public class SpruceTree(GenHelper helper, IChunk chunk) : BaseTree(helper, chunk, Material.SpruceLeaves, Material.SpruceLog, 9)
-{
-}
