02f94b513b0dfbedc5eaf2c055f6e0170223396e
jonpro03 <jonpro03@gmail.com>
2025-12-01 02:05:13 +0000

    fix: remove old tree generators

--- Obsidian/WorldData/Features/Trees/OakTree.cs
+++
@@ -1,7 +0,0 @@
-﻿using Obsidian.WorldData.Generators;
-
-namespace Obsidian.WorldData.Features.Trees;
-
-public class OakTree(GenHelper helper, IChunk chunk) : BaseTree(helper, chunk, Material.OakLeaves, Material.OakLog, 7)
-{
-}
--- Obsidian/WorldData/Features/Trees/TallBirchTree.cs
+++
@@ -1,7 +0,0 @@
-﻿using Obsidian.WorldData.Generators;
-
-namespace Obsidian.WorldData.Features.Trees;
-
-public class TallBirchTree(GenHelper helper, IChunk chunk) : BaseTree(helper, chunk, Material.BirchLeaves, Material.BirchLog, 11)
-{
-}
